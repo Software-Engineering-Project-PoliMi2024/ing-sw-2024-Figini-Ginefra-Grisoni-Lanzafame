49eae87a2bf1662cad5e480de2ac321031bb7cce
dedepivot <samuele.grisoni@mail.polimi.it>
2024-03-19 18:05:10 +0100

    feat: add Game boiler-plate code

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/tableReleted/Game.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/tableReleted/Game.java
@@ -1,15 +1,33 @@
 package it.polimi.ingsw.model.tableReleted;
 
+import java.util.LinkedList;
+
 public abstract class Game {
-    final int points;
+    private Deck<ObjectiveCard> objectiveCardDeck = new Deck<ObjectiveCard>(0);
+    private Deck<ResourceCard> resourceCardDeck = new Deck<ResourceCard>(2);
+    private Deck<GoldCard> goldCardDeck = new Deck<GoldCard>(2);
+    private Deck<StartingCard> startingCardDeck = new Deck<StartingCard>(0);
+    private List<User> users = new LinkedList<User>(); //played by
+    private User currentPlayer;
+    private final int numberOfPlayer;
+    public Game(int numberOfPlayer){
+        this.numberOfPlayer=numberOfPlayer;
+    }
+    public void addUser(User user){
 
-    public Game(){
-        points = 0;
     }
 
-    public int getPoints() {
-        return points;
+    public void removeUser(User user){
+
+    }
+    public List<User> getUsers() {
+        return users;
     }
 
+    public User getCurrentPlayer() {
+        return currentPlayer;
+    }
 
+    public void nextPlayer(){
+    }
 }
