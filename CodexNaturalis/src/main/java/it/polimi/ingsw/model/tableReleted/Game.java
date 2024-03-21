482184e8098c5bac04866266e7665c36a962d2c3
dedepivot <samuele.grisoni@mail.polimi.it>
2024-03-21 15:21:51 +0100

    chore: import User from player package

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/tableReleted/Game.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/tableReleted/Game.java
@@ -4,6 +4,9 @@ import it.polimi.ingsw.model.cardReleted.ObjectiveCard;
 import it.polimi.ingsw.model.cardReleted.ResourceCard;
 import it.polimi.ingsw.model.cardReleted.GoldCard;
 import it.polimi.ingsw.model.cardReleted.StartCard;
+import it.polimi.ingsw.model.playerReleted.User;
+
+import java.util.List;
 
 public abstract class Game {
     final private Deck<ObjectiveCard> objectiveCardDeck = new Deck<>(0);
@@ -14,6 +17,7 @@ public abstract class Game {
     private User currentPlayer;
     private final int numberOfPlayer;
     public Game(int numberOfPlayer){
+        users=null;
         this.numberOfPlayer=numberOfPlayer;
     }
     public void addUser(User user){
