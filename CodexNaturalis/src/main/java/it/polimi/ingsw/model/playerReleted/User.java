25defb132d37b6eeb6718164add71f6e235c7e5c
dedepivot <samuele.grisoni@mail.polimi.it>
2024-03-23 07:12:45 +0100

    chore: remove user frontier

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/playerReleted/User.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/playerReleted/User.java
@@ -3,25 +3,24 @@ package it.polimi.ingsw.model.playerReleted;
 public class User {
     private final String nickname;
     private final Codex userCodex;
-    private final Frontier userFrontier;
     private final Hand userHand;
 
     public User(String nickname){
         this.nickname = nickname;
         this.userCodex = new Codex();
-        this.userFrontier = new Frontier();
+        //this.userFrontier = new Frontier();
         this.userHand = new Hand();
     }
 
+    /** @return the user nickname*/
     public String getNickname(){
         return this.nickname;
     }
+    /** @return the user codex address*/
     public Codex getUserCodex(){
         return this.userCodex;
     }
-    public Frontier getUserFrontier(){
-        return this.userFrontier;
-    }
+    /** @return the user hand address*/
     public Hand getUserHand(){
         return this.userHand;
     }
