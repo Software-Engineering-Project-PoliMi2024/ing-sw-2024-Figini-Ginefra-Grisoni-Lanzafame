2c6c3b398f35c813590700679de525d1150aa057
dedepivot <samuele.grisoni@mail.polimi.it>
2024-03-26 15:18:02 +0100

    feat: add GoldCard

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cardReleted/GoldCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cardReleted/GoldCard.java
@@ -1,14 +1,41 @@
 package it.polimi.ingsw.model.cardReleted;
 
+import it.polimi.ingsw.model.playerReleted.Codex;
+
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class GoldCard extends CardInHand{
     final private Map<Resource, Integer> requirements;
     final private GoldCardPointMultiplier multiplier;
 
-    public GoldCard(){
-        super();
-        requirements = null;
-        multiplier = null;
+    /**
+     * The constructor of the class
+     * @param points set the number of points given by the card
+     * @param requirements it's a map of the requirements needed by the card and the number of them
+     * @param multiplier define
+     * @param frontCorner the front corners map
+     * @param permanentResource the resource in the midde of the card's back
+     */
+    public GoldCard(int points, Map<Resource, Integer> requirements, GoldCardPointMultiplier multiplier, Map<CardCorner, Collectable> frontCorner, Resource permanentResource){
+        super(permanentResource, points, frontCorner);
+        this.requirements = requirements;
+        this.multiplier = multiplier;
+    }
+
+    /**
+     * @return the requirements for placing the card
+     */
+    public Map<Resource, Integer> getRequirements() {
+        return requirements;
+    }
+
+    @Override
+    /*
+     * @return the amount of point given to the player by the card
+     */
+    public int getPoints(Codex codex) {
+        return this.getPoints()*multiplier.getMultiplier(codex);
     }
 }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cardReleted/GoldCardPointMultiplier.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cardReleted/GoldCardPointMultiplier.java
@@ -1,7 +1,6 @@
 package it.polimi.ingsw.model.cardReleted;
 
 import it.polimi.ingsw.model.playerReleted.Codex;
-import it.polimi.ingsw.model.playerReleted.Placement;
 
 public interface GoldCardPointMultiplier {
     public int getMultiplier(Codex codex);
