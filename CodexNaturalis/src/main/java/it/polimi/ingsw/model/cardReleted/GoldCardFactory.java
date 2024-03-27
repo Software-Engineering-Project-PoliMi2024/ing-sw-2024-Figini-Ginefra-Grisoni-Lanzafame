65a0eec9d1b950475384d2a48823d3bcaf32f15a
dedepivot <samuele.grisoni@mail.polimi.it>
2024-03-27 22:31:02 +0100

    feat: add GoldCardFactory functionality, fix some json typo

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cardReleted/GoldCardFactory.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cardReleted/GoldCardFactory.java
@@ -1,8 +1,12 @@
 package it.polimi.ingsw.model.cardReleted;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 
 /**
@@ -12,8 +16,41 @@ public class GoldCardFactory extends AbstractCardFactory<GoldCard>{
     private final Queue<GoldCard> deckBuilder = new LinkedList<>();
     @Override
     public Queue<GoldCard> getCards() {
-        JsonArray ResourceCards = getCardArray("GoldCards");
-        //cardCreationLogic
-        return null; //placeHolder
+        JsonArray GoldCards = getCardArray("GoldCards");
+        for(int i =0; i<GoldCards.size(); i++){
+            JsonObject card = GoldCards.get(i).getAsJsonObject();
+            getPointMultiplier(card);
+            deckBuilder.add(new GoldCard(card.get("points").getAsInt(), getRequirmentMap(card),
+                    getPointMultiplier(card),getFrontCornerMap(card),getPermanentResource(card)));
+        }
+        return deckBuilder;
+    }
+
+    private Map<Resource, Integer> getRequirmentMap(JsonObject card){
+        Map<Resource, Integer> requirmentMap = new HashMap<>();
+        JsonObject requirementsList = card.getAsJsonObject("requirements");
+        //For each "element" get the key in UpperCase and use the enum toString to convert it
+        for (Map.Entry<String, JsonElement> element : requirementsList.entrySet()){
+            requirmentMap.put(Resource.valueOf(element.getKey().toUpperCase()),
+                    element.getValue().getAsInt());
+        }
+        return requirmentMap;
+    }
+
+    private GoldCardPointMultiplier getPointMultiplier(JsonObject card){
+        String multiplierType = card.get("multiplier").getAsString().toUpperCase();
+        if(multiplierType.isEmpty()){
+            return null;
+        }else if(multiplierType.equals("CORNER")){
+            return new CoveredCornersCardPointMultiplier();
+        }else{
+            return new WritingMaterialsCardPointMultiplier(WritingMaterial.valueOf(multiplierType));
+        }
+    }
+    public static void main(String[] arg){
+        GoldCardFactory factory = new GoldCardFactory();
+        factory.getCards();
     }
 }
+
+
