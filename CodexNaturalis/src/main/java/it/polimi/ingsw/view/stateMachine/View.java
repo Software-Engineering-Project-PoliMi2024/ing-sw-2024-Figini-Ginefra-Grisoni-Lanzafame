b67f411a2acf0fdc55fa910c65d64719b8ee06a7
figinii <matteo5.figini@mail.polimi.it>
2024-04-05 16:21:47 +0200

    feat: adding base view

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/stateMachine/View.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.view.stateMachine;
+
+public abstract class View {
+    public void displayCodex(){}
+    void clearActions(){}
+    void clearVisualizations(){}
+    void render(){}
+
+}
