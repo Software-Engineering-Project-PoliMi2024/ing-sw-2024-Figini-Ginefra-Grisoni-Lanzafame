164f59c3268d2798a8151e27176e259f64bb1aba
dedepivot <samuele.grisoni@mail.polimi.it>
2024-04-14 20:31:38 +0200

    feat: add LeaveGameMsg, its answer and notification

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller/socket/messages/actionMessages/LeaveGameMsg.java
@@ -0,0 +1,44 @@
+package it.polimi.ingsw.controller.socket.messages.actionMessages;
+
+import it.polimi.ingsw.controller.socket.messages.serverMessages.answerMessages.LeaveGameMsgAnswer;
+import it.polimi.ingsw.controller.socket.messages.serverMessages.notificationMessages.LeaveGameNotificationMsg;
+import it.polimi.ingsw.controller.socket.server.SocketClientHandler;
+import it.polimi.ingsw.model.playerReleted.User;
+import it.polimi.ingsw.model.tableReleted.EmptyMatchException;
+import it.polimi.ingsw.model.tableReleted.UserNotFoundException;
+
+import java.io.IOException;
+
+public class LeaveGameMsg extends ActionMsg{
+    /**
+     * The constructor of the class
+     */
+    public LeaveGameMsg(){
+        super();
+    }
+
+    /**
+     * Remove the player from the current match, the observerList
+     * Send a LeaveGameMsgAnswer and a LeaveGameNotificationMsg
+     * @param socketClientHandler the ClientHandler who received the ActionMsg from the client
+     * @throws IOException
+     */
+    @Override
+    public void processMessage(SocketClientHandler socketClientHandler) throws IOException {
+        try {
+            socketClientHandler.getGame().getGameParty().removeUser(socketClientHandler.getUser());
+            socketClientHandler.setGame(null);
+
+            socketClientHandler.getGame().getGameParty().detach(socketClientHandler);
+            socketClientHandler.sendServerMessage(new LeaveGameNotificationMsg(socketClientHandler.getUser().getNickname()));
+
+            socketClientHandler.sendServerMessage(new LeaveGameMsgAnswer(this));
+
+            System.out.println("User " + socketClientHandler.getUser().getNickname() + " joined game " + socketClientHandler.getGame().getName());
+            System.out.println("Active Players:" + socketClientHandler.getGame().getGameParty().getUsersList().stream().map(User::getNickname).reduce("", (a, b) -> a + " " + b));
+
+        } catch (EmptyMatchException | UserNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller/socket/messages/serverMessages/notificationMessages/LeaveGameNotificationMsg.java
@@ -0,0 +1,35 @@
+package it.polimi.ingsw.controller.socket.messages.serverMessages.notificationMessages;
+
+import it.polimi.ingsw.controller.socket.client.SocketServerHandler;
+import it.polimi.ingsw.controller.socket.messages.serverMessages.ServerMsg;
+
+
+public class LeaveGameNotificationMsg extends ServerMsg {
+
+    private final String nickname;
+
+    /**
+     * The constructor of the class
+     * @param nickname of the player who is leaving
+     */
+    public LeaveGameNotificationMsg(String nickname){
+        this.nickname = nickname;
+    }
+
+    /**
+     * @return the player's nickname
+     */
+    public String getNickname() {
+        return nickname;
+    }
+
+    /**
+     * Update the view
+     * @param socketServerHandler who received an answer/notification from the server
+     */
+    @Override
+    public void processMessage(SocketServerHandler socketServerHandler) {
+        //view.update
+        System.out.println(nickname + " left the game.");
+    }
+}
