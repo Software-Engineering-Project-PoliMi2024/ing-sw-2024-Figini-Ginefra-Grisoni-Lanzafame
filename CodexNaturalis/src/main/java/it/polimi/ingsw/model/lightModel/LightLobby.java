fe7f335b13a2f7ed1fe05296daa5a70e36f0a138
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-04-25 13:49:51 +0200

    refactor: to record

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/lightModel/LightLobby.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/lightModel/LightLobby.java
@@ -8,38 +8,39 @@ import java.util.List;
 /**
  * This class is a container for the list of lobbies.
  */
-public class LightLobby implements ModelDifferentiable<String> {
-    private final List<String> nicknames;
-    private final String name;
-
+public record LightLobby(List<String> nicknames, String name) implements ModelDifferentiable<String> {
     /**
      * Creates a LightLobby object.
+     *
      * @param nicknames the list of nicknames
-     * @param name the name of the lobby
+     * @param name      the name of the lobby
      */
-    public LightLobby(List<String> nicknames, String name) {
-        this.nicknames = nicknames;
-        this.name = name;
+    public LightLobby {
     }
 
     /**
      * Returns the list of nicknames.
+     *
      * @return the list of nicknames
      */
-    public List<String> getNicknames() {
+    @Override
+    public List<String> nicknames() {
         return nicknames;
     }
 
     /**
      * Returns the name of the lobby.
+     *
      * @return the name of the lobby
      */
-    public String getName() {
+    @Override
+    public String name() {
         return name;
     }
 
     /**
      * Applies the differences to the list of nicknames.
+     *
      * @param diff the differences to apply
      */
     @Override
