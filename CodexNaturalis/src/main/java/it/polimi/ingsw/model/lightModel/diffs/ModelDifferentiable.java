94716e07dd435761375fa911c7d871fa46f27fe7
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-04-25 12:55:59 +0200

    fix: add public

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/lightModel/diffs/ModelDifferentiable.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/lightModel/diffs/ModelDifferentiable.java
@@ -4,6 +4,6 @@ package it.polimi.ingsw.model.lightModel.diffs;
  * This interface is used to apply differences to a model.
  * @param <DiffType> the type of the elements that have been added or removed
  */
-public interface ModelDifferentiable <DiffType>{
-    void applyDiff(ModelDiff<DiffType> diff);
+public interface ModelDifferentiable<DiffType>{
+    public void applyDiff(ModelDiff<DiffType> diff);
 }
