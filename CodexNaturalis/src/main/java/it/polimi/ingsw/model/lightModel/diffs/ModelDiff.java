65aa087ecd6702d3ebbd11585e05c32c34b64648
dedepivot <samuele.grisoni@mail.polimi.it>
2024-04-25 14:48:31 +0200

    fix: fix a major error in ModelDiff

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/lightModel/diffs/ModelDiff.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/lightModel/diffs/ModelDiff.java
@@ -1,12 +1,25 @@
 package it.polimi.ingsw.model.lightModel.diffs;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * This class is a container for the differences between two models.
- * @param addList list of elements that have been added
- * @param removeList list of elements that have been removed
  * @param <DiffType> the type of the elements that have been added or removed
  */
-public record ModelDiff<DiffType>(List<DiffType> addList, List<DiffType> removeList){
+public class ModelDiff<DiffType>{
+    private final List<DiffType> addList;
+    private final List<DiffType> removeList;
+
+    public ModelDiff(){
+        addList = new ArrayList<>();
+        removeList = new ArrayList<>();
+    }
+    public List<DiffType> getAddList() {
+        return addList;
+    }
+
+    public List<DiffType> getRemoveList() {
+        return removeList;
+    }
 }
