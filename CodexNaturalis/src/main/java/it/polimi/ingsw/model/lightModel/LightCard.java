baaf2939fd17d894cc034fcfb66645d5471bc045
figinii <matteo5.figini@mail.polimi.it>
2024-04-25 12:36:04 +0200

    feat: add lightModel

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/lightModel/LightCard.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.model.lightModel;
+
+public class LightCard {
+    final private int id;
+
+    public LightCard(int id){
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+}
