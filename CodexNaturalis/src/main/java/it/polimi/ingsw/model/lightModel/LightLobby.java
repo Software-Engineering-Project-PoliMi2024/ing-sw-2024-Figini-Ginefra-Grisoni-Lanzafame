2d5d56afa3ed88ce58689687d3db9ba3e91f8600
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-04-25 13:48:26 +0200

    docs: add documentation

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/lightModel/LightLobby.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/lightModel/LightLobby.java
@@ -5,23 +5,43 @@ import it.polimi.ingsw.model.lightModel.diffs.ModelDifferentiable;
 
 import java.util.List;
 
+/**
+ * This class is a container for the list of lobbies.
+ */
 public class LightLobby implements ModelDifferentiable<String> {
     private final List<String> nicknames;
     private final String name;
 
+    /**
+     * Creates a LightLobby object.
+     * @param nicknames the list of nicknames
+     * @param name the name of the lobby
+     */
     public LightLobby(List<String> nicknames, String name) {
         this.nicknames = nicknames;
         this.name = name;
     }
 
+    /**
+     * Returns the list of nicknames.
+     * @return the list of nicknames
+     */
     public List<String> getNicknames() {
         return nicknames;
     }
 
+    /**
+     * Returns the name of the lobby.
+     * @return the name of the lobby
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Applies the differences to the list of nicknames.
+     * @param diff the differences to apply
+     */
     @Override
     public void applyDiff(ModelDiff<String> diff) {
         nicknames.removeAll(diff.removeList());
