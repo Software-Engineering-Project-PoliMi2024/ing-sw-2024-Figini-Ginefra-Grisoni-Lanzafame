93a481be08b7a990389bcee54222c9d919a3a463
figinii <matteo5.figini@mail.polimi.it>
2024-04-29 21:06:32 +0200

    rmv: unused classes

--- CodexNaturalis/src/main/java/it/polimi/ingsw/designPatterns/Observer/Observed.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.designPatterns.Observer;
-
-import java.util.function.Function;
-
-public interface Observed {
-    public void attach(Observer observer);
-    public void detach(Observer observer);
-    public void notifyObservers();
-}
--- CodexNaturalis/src/main/java/it/polimi/ingsw/designPatterns/Observer/Observer.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.ingsw.designPatterns.Observer;
-
-import java.util.function.Function;
-
-public interface Observer {
-    public void update();
-}
