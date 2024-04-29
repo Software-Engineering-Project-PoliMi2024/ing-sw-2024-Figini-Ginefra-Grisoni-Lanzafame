97599d5298b7921e7965d1eb20554a2d7022ab68
dedepivot <samuele.grisoni@mail.polimi.it>
2024-04-29 12:34:27 +0200

    feat: add login method in ServerImplementation

--- CodexNaturalis/src/main/java/it/polimi/ingsw/controller2/LogsFromServer.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller2/LogsFromServer.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.controller2;
 
 public enum LogsFromServer {
+    NAME_TAKEN("The nickname is already present in the server"),
+    SERVER_JOINED("Server Joined"),
     LOBBY_IS_FULL("The lobby is full"),
     LOBBY_JOINED("Lobby joined"),
     LOBBY_LEFT("Lobby left"),
--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/ViewState.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/ViewState.java
@@ -3,8 +3,8 @@ package it.polimi.ingsw.view;
 public enum ViewState {
     SERVER_CONNECTION, //the state that asks for the server ip and port
     CONNECT_FORM, //the state that asks for the nickname
-    JOIN_GAME,
-    CREATE_GAME,
+    JOIN_LOBBY,
+    CREATE_LOBBY,
     LOBBY,
     CHOOSE_START_CARD,
     SELECT_OBJECTIVE,
