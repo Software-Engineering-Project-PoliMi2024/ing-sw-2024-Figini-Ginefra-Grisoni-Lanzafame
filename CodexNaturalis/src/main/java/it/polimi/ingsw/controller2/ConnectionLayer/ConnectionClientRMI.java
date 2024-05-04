f26861fea4a0c3700f30d1c56da3053aef5612da
dedepivot <samuele.grisoni@gmail.com>
2024-05-04 10:48:39 +0200

    docs: add documentation

--- CodexNaturalis/src/main/java/it/polimi/ingsw/controller2/ConnectionLayer/ConnectionClientRMI.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller2/ConnectionLayer/ConnectionClientRMI.java
@@ -11,7 +11,13 @@ import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class ConnectionClientRMI implements ConnectionLayerClient{
-
+    /**
+     * Establishes a connection with the RMI server located at the specified IP address and port.
+     * This method enables communication between a client and the server through RMI.
+     * @param ip The IP address of the RMI server.
+     * @param port The port number where the RMI server is listening.
+     * @param view the view of the client
+     */
     public void connect(String ip, int port, ViewInterface view) {
         try {
             Registry registry = LocateRegistry.getRegistry(ip, port);
@@ -19,11 +25,14 @@ public class ConnectionClientRMI implements ConnectionLayerClient{
             try {
                 connect = (ConnectionLayerServer) registry.lookup("connect");
             }catch (NotBoundException b){
+                System.out.println("No method connect found on the Server");
+                view.log(LogsFromServer.CONNECTION_ERROR.getMessage());
                 b.printStackTrace();
             }
+            //expose client's view to the server
             ViewInterface viewStub = (ViewInterface) UnicastRemoteObject.exportObject(view, 0);
             connect.connect(viewStub);
-        } catch (RemoteException r) {
+        } catch (RemoteException r) { //serverOfflient
             r.printStackTrace();
             try {
                 view.log(LogsFromServer.CONNECTION_ERROR.getMessage());
--- CodexNaturalis/src/main/java/it/polimi/ingsw/controller2/ConnectionLayer/ConnectionServerRMI.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller2/ConnectionLayer/ConnectionServerRMI.java
@@ -15,13 +15,28 @@ import java.rmi.server.UnicastRemoteObject;
 
 public class ConnectionServerRMI implements ConnectionLayerServer, Serializable{
     private final MultiGame multiGame;
+
+    /**
+     * The constructor of the class
+     * @param multiGame the wrapper for the Model present in the server
+     */
     public ConnectionServerRMI(MultiGame multiGame){
         this.multiGame = multiGame;
     }
+
+    /**
+     * Establishes a connection with a client by initializing the necessary components on the server side.
+     *
+     * @param view the view of the client which is trying to connect
+     * @throws RemoteException if a communication-related exception occurs during the execution of this method.
+     */
     public void connect(ViewInterface view) throws RemoteException {
+        //Create a ServerModelContoller for the new client
         ControllerInterface controller = new ServerModelController(multiGame, view);
+        //expose the controller to the client
         ControllerInterface controllerStub = (ControllerInterface) UnicastRemoteObject.exportObject(controller, 0);
         view.postConnectionInitialization(controllerStub);
+        
         view.log(LogsFromServer.CONNECTION_SUCCESS.getMessage());
         view.transitionTo(ViewState.LOGIN_FORM);
     }
