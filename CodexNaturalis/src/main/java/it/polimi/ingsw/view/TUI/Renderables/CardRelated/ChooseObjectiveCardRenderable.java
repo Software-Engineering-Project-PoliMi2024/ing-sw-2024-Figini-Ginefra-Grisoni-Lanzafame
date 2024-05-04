a721f3c1196d9d1cdc47adfa6151691baebe0db9
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-05-04 23:05:53 +0200

    doc: add doc

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/TUI/Renderables/CardRelated/ChooseObjectiveCardRenderable.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/TUI/Renderables/CardRelated/ChooseObjectiveCardRenderable.java
@@ -15,12 +15,25 @@ import it.polimi.ingsw.view.TUI.inputs.CommandPromptResult;
 
 import java.rmi.RemoteException;
 
+/**
+ * This class is a Renderable that can render a card and prompt the player to choose between two objective cards.
+ */
 public class ChooseObjectiveCardRenderable extends CardRenderable {
-
+    /**
+     * Creates a new ChooseObjectiveCardRenderable.
+     * @param name The name of the renderable.
+     * @param museum The card museum to use.
+     * @param game The lightGame to render.
+     * @param relatedCommands The commands related to this renderable.
+     * @param controller The controller to interact with.
+     */
     public ChooseObjectiveCardRenderable(String name, CardMuseum museum, LightGame game, CommandPrompt[] relatedCommands, ControllerInterface controller){
         super(name, museum, game, CardFace.FRONT, relatedCommands, controller);
     }
 
+    /**
+     * Renders the two objective cards.
+     */
     public void render(){
         String firstOptionNumber = new DecoratedString("[1]", StringStyle.BOLD).toString();
         PromptStyle.printInABox("Option " + firstOptionNumber, CardTextStyle.getCardWidth() * 2);
@@ -35,6 +48,10 @@ public class ChooseObjectiveCardRenderable extends CardRenderable {
         this.renderCard(secondOption);
     }
 
+    /**
+     * Updates the renderable based on the command prompt result.
+     * @param command The command prompt result.
+     */
     public void updateCommand(CommandPromptResult command){
         switch (command.getCommand()) {
             case CommandPrompt.CHOOSE_OBJECTIVE_CARD:
