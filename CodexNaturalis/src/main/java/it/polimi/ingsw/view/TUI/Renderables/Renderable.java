0c9eeddd133fb91a1b923d4a856e69a4a05f4d35
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-05-04 18:25:34 +0200

    doc: add doc

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/TUI/Renderables/Renderable.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/TUI/Renderables/Renderable.java
@@ -8,18 +8,23 @@ import it.polimi.ingsw.view.TUI.observers.InputObserver;
 
 import java.io.Serializable;
 
+/**
+ * Abstract class that represents a renderable object that is an object that can be rendered on the screen and can handle inputs
+ */
 public abstract class Renderable implements InputObserver, CommandObserver, Serializable {
     private boolean active = false;
-
     private final CommandPrompt[] relatedCommands;
-
     protected final ControllerInterface controller;
-
     private final String name;
 
+    /**
+     * Constructor
+     * @param name the name of the renderable
+     * @param relatedCommands the commands that are related to this renderable
+     * @param controller the controller that will handle the inputs
+     */
     public Renderable(String name, CommandPrompt[] relatedCommands, ControllerInterface controller){
         this.name = name;
-
         this.relatedCommands = relatedCommands;
 
         this.controller = controller;
@@ -31,26 +36,54 @@ public abstract class Renderable implements InputObserver, CommandObserver, Seri
         }
     }
 
+    /**
+     * Method that renders the object on the screen
+     */
     public abstract void render();
 
+    /**
+     * Method that updates the renderable on terminal input
+     * @param input the input to update
+     */
     public void updateInput(String input){
         throw new UnsupportedOperationException("Update without input not supported");
     };
 
+    /**
+     * Sets the renderable as active or not
+     * @param active true if the renderable is active, false otherwise
+     */
     public void setActive(boolean active){
         this.active = active;
     }
 
+    /**
+     * Method that updates the renderable on the result of an observed CommandPrompt
+     * @param command the command to update
+     */
     public void updateCommand(CommandPromptResult command){
     }
 
+    /**
+     * Method that returns if the renderable is active
+     * @return true if the renderable is active, false otherwise
+     */
     public boolean isActive(){
         return this.active;
     }
 
+    /**
+     * Method that returns the name of the renderable
+     * @return the name of the renderable
+     */
     public String getName(){
         return name;
     }
+
+    /**
+     * Method that returns the related commands of the renderable
+     * @return the related commands of the renderable
+     */
     public CommandPrompt[] getRelatedCommands(){
         return relatedCommands;
     }
