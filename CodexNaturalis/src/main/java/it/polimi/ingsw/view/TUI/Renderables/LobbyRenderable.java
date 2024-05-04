118b9c88cfe40ce91bd84954faea2471825fae46
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-05-04 18:27:50 +0200

    doc: add doc

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/TUI/Renderables/LobbyRenderable.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/TUI/Renderables/LobbyRenderable.java
@@ -9,15 +9,27 @@ import it.polimi.ingsw.view.TUI.inputs.CommandPromptResult;
 import java.rmi.RemoteException;
 import java.util.List;
 
+/**
+ * This class is a Renderable that represents a lobby.
+ */
 public class LobbyRenderable extends Renderable {
-    private LightLobby lightLobby;
-    private String userInput = "";
+    private final LightLobby lightLobby;
 
+    /**
+     * Creates a new LobbyRenderable.
+     * @param name The name of the renderable.
+     * @param lightLobby The lightLobby to render.
+     * @param relatedCommands The commands related to this renderable.
+     * @param controller The controller to interact with.
+     */
     public LobbyRenderable(String name, LightLobby lightLobby, CommandPrompt[] relatedCommands, ControllerInterface controller) {
         super(name, relatedCommands, controller);
         this.lightLobby = lightLobby;
     }
 
+    /**
+     * Renders the lobby.
+     */
     @Override
     public void render() {
         String lobbyName = lightLobby.name();
@@ -31,6 +43,10 @@ public class LobbyRenderable extends Renderable {
         PromptStyle.printListInABox("Lobby - " + lobbyName, nicknames, 70, 1);
     }
 
+    /**
+     * Updates the renderable based on the command prompt result.
+     * @param answer The command prompt result.
+     */
     @Override
     public void updateCommand(CommandPromptResult answer) {
         switch (answer.getCommand()) {
