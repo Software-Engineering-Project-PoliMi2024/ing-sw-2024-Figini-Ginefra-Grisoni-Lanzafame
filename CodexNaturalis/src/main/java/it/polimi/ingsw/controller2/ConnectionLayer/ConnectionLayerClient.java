87be9d712b508e909bd89d8216e72cbe99246f70
figinii <matteo5.figini@mail.polimi.it>
2024-05-06 16:06:10 +0200

    feat: add erase method

--- CodexNaturalis/src/main/java/it/polimi/ingsw/controller2/ConnectionLayer/ConnectionLayerClient.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller2/ConnectionLayer/ConnectionLayerClient.java
@@ -1,12 +1,21 @@
 package it.polimi.ingsw.controller2.ConnectionLayer;
 
+import it.polimi.ingsw.lightModel.diffs.nuclearDiffs.FatManLobbyList;
+import it.polimi.ingsw.lightModel.diffs.nuclearDiffs.GadgetGame;
+import it.polimi.ingsw.lightModel.diffs.nuclearDiffs.LittleBoyLobby;
 import it.polimi.ingsw.view.ViewInterface;
 
 import java.io.Serializable;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface ConnectionLayerClient extends Remote, Serializable {
-    void connect(String ip, int port, ViewInterface view);
-    void ping() throws RemoteException;
+public abstract class ConnectionLayerClient implements Remote, Serializable {
+    private ViewInterface view;
+    public void connect(String ip, int port, ViewInterface view){}
+    public void ping() throws RemoteException{}
+    public void erase() throws RemoteException{
+        view.updateLobbyList(new FatManLobbyList());
+        view.updateLobby(new LittleBoyLobby());
+        view.updateGame(new GadgetGame());
+    }
 }
