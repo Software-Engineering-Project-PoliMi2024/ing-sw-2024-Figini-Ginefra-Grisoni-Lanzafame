84e8c689b85ca82f65ae88c1c5bbeeb29b16d7b7
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-05-06 16:49:31 +0200

    test: refine test with all directions

--- CodexNaturalis/src/test/java/it/polimi/ingsw/view/TUI/Renderables/CodexRelated/CodexRenderableTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/view/TUI/Renderables/CodexRelated/CodexRenderableTest.java
@@ -38,11 +38,15 @@ class CodexRenderableTest {
         diff.apply(lightGame);
 
         List<LightPlacement> placements = List.of(new LightPlacement[]{
-                new LightPlacement(new Position(0, 0), new LightCard(81), CardFace.FRONT)
+                new LightPlacement(new Position(0, 0), new LightCard(81), CardFace.FRONT),
+                new LightPlacement(new Position(-1, -1), new LightCard(82), CardFace.BACK),
+                new LightPlacement(new Position(1, -1), new LightCard(85), CardFace.FRONT),
+                new LightPlacement(new Position(-1, 1), new LightCard(86), CardFace.BACK),
+
         });
 
         List<Position> positions = List.of(new Position[]{
-                new Position(1, 1)
+                new Position(1, 1),
         });
 
 
