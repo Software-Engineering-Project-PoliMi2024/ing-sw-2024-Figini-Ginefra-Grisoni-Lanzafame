ee624cfd8d25587e3cd457c2c20ccfbf33f873c6
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-05-06 14:04:38 +0200

    chore: removed outdated classes

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/tableReleted/EmptyMatchException.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.model.tableReleted;
-
-import java.io.Serializable;
-
-public class EmptyMatchException extends Exception implements Serializable {
-    public EmptyMatchException(String errorMessage){
-        super(errorMessage);
-    }
-}
--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/tableReleted/FullMatchException.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.model.tableReleted;
-
-import java.io.Serializable;
-
-public class FullMatchException extends Exception implements Serializable {
-    public FullMatchException(String errorMessage){
-        super(errorMessage);
-    }
-}
