677a3468ee28bbda10dc168601cefcb2b25fc798
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-05-06 15:57:31 +0200

    feat: add addSecretObjectiveChoice method

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/playerReleted/Hand.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/playerReleted/Hand.java
@@ -86,4 +86,11 @@ public class Hand implements Serializable {
     public List<ObjectiveCard> getSecretObjectiveChoices() {
         return secretObjectiveChoices == null ? null : new ArrayList<>(secretObjectiveChoices);
     }
+
+    public void addSecretObjectiveChoice(ObjectiveCard objectiveCard){
+        if(secretObjectiveChoices == null){
+            secretObjectiveChoices = new LinkedList<>();
+        }
+        secretObjectiveChoices.add(objectiveCard);
+    }
 }
