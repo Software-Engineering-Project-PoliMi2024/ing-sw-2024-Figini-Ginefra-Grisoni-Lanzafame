58a767b08a8a40d553728b21e7ad8fc3bfa9b069
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-05-06 16:26:43 +0200

    chore: removed outdated classes

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/States/joinGameState.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.ingsw.view.States;
-
-import it.polimi.ingsw.view.States.viewState;
-
-public abstract class joinGameState extends viewState {
-
-}
--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/States/viewState.java
+++
@@ -1,6 +0,0 @@
-package it.polimi.ingsw.view.States;
-
-public abstract class viewState {
-    public static void run() {}
-
-}
