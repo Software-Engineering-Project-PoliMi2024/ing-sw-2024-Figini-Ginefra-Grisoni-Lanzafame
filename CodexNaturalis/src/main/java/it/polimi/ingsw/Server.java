83c84381306673a49ff22937b0d016c192bf7130
figinii <matteo5.figini@mail.polimi.it>
2024-05-07 19:14:18 +0200

    feat: add possibility to not show ip

--- CodexNaturalis/src/main/java/it/polimi/ingsw/Server.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Server.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw;
 import it.polimi.ingsw.controller.socket.server.SocketServer;
 import it.polimi.ingsw.controller2.ConnectionLayer.ConnectionLayerServer;
 import it.polimi.ingsw.controller2.ConnectionLayer.ConnectionServerRMI;
+import it.polimi.ingsw.controller2.ConnectionLayer.VirtualRMI.VirtualControllerRMI;
 import it.polimi.ingsw.model.MultiGame;
 import it.polimi.ingsw.controller.RMI.ServerRMI;
 import it.polimi.ingsw.view.TUI.Printing.Printer;
@@ -13,20 +14,34 @@ import java.net.Socket;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Scanner;
 
 /**
  * Starts the server, create an object of MultiGame, starts his SocketServer Thread
  */
 public class Server {
-
     public static void main(String[] args) {
         System.out.println("SERVER STARTED! 🚀🚀🚀");
+        System.out.println("show IP? (possible only with internet connection)");
 
-        try (Socket socket = new Socket()) {
-            socket.connect(new InetSocketAddress("google.com", 80));
-            Printer.println("IP: " + socket.getLocalAddress().getHostAddress());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        System.out.println("0 = no / 1 = yes");
+
+        Scanner scanner = new Scanner(System.in);
+        String choice = scanner.nextLine();
+
+        while(!choice.matches("[01]")) {
+            System.out.println("Invalid choice! Please choose again.");
+            choice = scanner.nextLine();
+        }
+        int choiceInt = Integer.parseInt(choice);
+
+        if (choiceInt == 1) {
+            try (Socket socket = new Socket()) {
+                socket.connect(new InetSocketAddress("google.com", 80));
+                Printer.println("IP: " + socket.getLocalAddress().getHostAddress());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         MultiGame multiGame = new MultiGame();
