e27f301042fdd50d31a2798a0ec6965b5d2459a3
Teo <teo.lanzafme@mail.polimi.it>
2024-05-07 13:15:14 +0200

    feat: LeaderboardRenderable

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/TUI/Renderables/LeaderboardRenderable.java
@@ -0,0 +1,58 @@
+package it.polimi.ingsw.view.TUI.Renderables;
+
+import it.polimi.ingsw.controller2.ControllerInterface;
+import it.polimi.ingsw.lightModel.lightTableRelated.LightGame;
+import it.polimi.ingsw.view.TUI.Styles.PromptStyle;
+import it.polimi.ingsw.view.TUI.inputs.CommandPrompt;
+import it.polimi.ingsw.view.TUI.inputs.CommandPromptResult;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class LeaderboardRenderable extends Renderable {
+    private final LightGame lightGame;
+
+    public LeaderboardRenderable(String name, LightGame lightGame, CommandPrompt[] relatedCommands, ControllerInterface controller) {
+        super(name, relatedCommands, controller);
+        this.lightGame = lightGame;
+    }
+
+    @Override
+    public void render() {
+        Map<String, Integer> scores = lightGame.getCodexMap().entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().getPoints()));
+
+        String leaderboard = scores.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .map(e -> e.getKey() + ": " + e.getValue())
+                .collect(Collectors.joining("\n"));
+
+        PromptStyle.printInABox("Leaderboard", 70);
+        PromptStyle.printListInABox("Player Scores", scores.keySet().stream().map(k -> k + " - " + scores.get(k)).toList(), 70, 1);
+    }
+
+
+    @Override
+    public void updateCommand(CommandPromptResult answer) {
+        switch (answer.getCommand()) {
+            case DISPLAY_LEADERBOARD:
+                this.render();
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+
+/*} public void render() {
+    this.scores = getLightGame().getCodexMap().values().stream().mapToInt(LightCodex::getPoints);
+    List<String> scoreDisplay = scores.entrySet().stream()
+            .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+            .map(e -> e.getKey() + ": " + e.getValue())
+            .collect(Collectors.toList());
+
+    System.out.println(PromptStyle.Title);  // Optional: Adds a title or header if you have any defined for sections
+    PromptStyle.printListInABox("Leaderboard", scoreDisplay, 50, 1);
+}
+ */
