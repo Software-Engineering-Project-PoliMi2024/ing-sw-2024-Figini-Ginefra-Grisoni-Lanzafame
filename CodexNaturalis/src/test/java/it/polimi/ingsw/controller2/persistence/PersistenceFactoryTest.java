f7710db58c749caf6a8da0bea7777657b98b79ba
figinii <matteo5.figini@mail.polimi.it>
2024-05-07 22:25:35 +0200

    feat: add test games load from saves

--- CodexNaturalis/src/test/java/it/polimi/ingsw/controller2/persistence/PersistenceFactoryTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/controller2/persistence/PersistenceFactoryTest.java
@@ -1,9 +1,14 @@
 package it.polimi.ingsw.controller2.persistence;
 
 import it.polimi.ingsw.model.MultiGame;
+import it.polimi.ingsw.model.playerReleted.User;
 import it.polimi.ingsw.model.tableReleted.Game;
 import it.polimi.ingsw.model.tableReleted.Lobby;
 import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+import java.io.File;
+import java.util.Objects;
 
 class persistenceFactoryTest {
 
@@ -14,10 +19,35 @@ class persistenceFactoryTest {
         lobby.addUserName("gianni1");
         lobby.addUserName("gianni2");
         PersistenceFactory.save(multiGame.createGame(lobby));
+
+        File file = new File("gameSaves/testGame.ser");
+        assert file.exists();
+        file.delete();
     }
 
     @Test
     void load() {
+        MultiGame multiGame = new MultiGame();
+        Lobby lobby = new Lobby(3, "gianni", "testGame");
+        lobby.addUserName("gianni1");
+        lobby.addUserName("gianni2");
+        PersistenceFactory.save(multiGame.createGame(lobby));
+
+        MultiGame multiGame1 = new MultiGame();
+
+        Game gameLoadedFromSave = multiGame1.getGameByName("testGame");
+        System.out.println(gameLoadedFromSave);
+        File file = new File("gameSaves/testGame.ser");
+        assert file.exists();
+        file.delete();
+        assert Objects.equals(gameLoadedFromSave.getName(), "testGame");
+        assert gameLoadedFromSave.getGameParty().getUsersList().size() == 3;
+        assert gameLoadedFromSave.getGameParty().getUsersList().stream().map(User::getNickname).toList().contains("gianni");
+        assert gameLoadedFromSave.getGameParty().getUsersList().stream().map(User::getNickname).toList().contains("gianni1");
+        assert gameLoadedFromSave.getGameParty().getUsersList().stream().map(User::getNickname).toList().contains("gianni2");
+
+        MultiGame multiGame2 = new MultiGame();
+        assert multiGame2.getGameByName("testGame") == null;
     }
 
     @Test
@@ -29,6 +59,8 @@ class persistenceFactoryTest {
         Game gameCreated = multiGame.createGame(lobby);
         PersistenceFactory.save(gameCreated);
         PersistenceFactory.deleteGameSave(gameCreated);
-        assert true;
+
+        File file = new File("gameSaves/testGame.ser");
+        assert !file.exists();
     }
 }
