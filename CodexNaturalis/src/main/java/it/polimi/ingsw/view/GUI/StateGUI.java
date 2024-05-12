307ab358fa913890a9f98ca13738807aad4c4bf5
figinii <matteo5.figini@mail.polimi.it>
2024-05-12 17:25:06 +0200

    feat: add JOIN_LOBBY state

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/StateGUI.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/StateGUI.java
@@ -10,7 +10,9 @@ import java.util.Objects;
 
 public enum StateGUI {
     SERVER_CONNECTION(ViewState.SERVER_CONNECTION, Root.SERVER_CONNECTION_FORM),
-    LOGIN_FORM(ViewState.LOGIN_FORM, Root.LOGIN_FORM),;
+    LOGIN_FORM(ViewState.LOGIN_FORM, Root.LOGIN_FORM),
+    JOIN_LOBBY(ViewState.JOIN_LOBBY, Root.LOBBY_LIST),
+    LOBBY(ViewState.LOBBY, Root.LOBBY),;
 
     private final ViewState referenceState;
     private final Root referenceRoot;
