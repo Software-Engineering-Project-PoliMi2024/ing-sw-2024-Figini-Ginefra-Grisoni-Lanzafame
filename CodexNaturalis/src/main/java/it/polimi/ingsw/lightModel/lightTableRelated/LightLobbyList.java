9169c57c7e16784e8e1d6cacbf7cbc682e6b4319
figinii <matteo5.figini@mail.polimi.it>
2024-05-13 00:02:55 +0200

    refactor: LightLobbyList is now composed of an ObservableList

--- CodexNaturalis/src/main/java/it/polimi/ingsw/lightModel/lightTableRelated/LightLobbyList.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/lightModel/lightTableRelated/LightLobbyList.java
@@ -2,6 +2,8 @@ package it.polimi.ingsw.lightModel.lightTableRelated;
 
 import it.polimi.ingsw.lightModel.Differentiable;
 import it.polimi.ingsw.lightModel.lightTableRelated.LightLobby;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,37 +12,40 @@ import java.util.List;
  * This class is a container for the list of lobbies.
  */
 public class LightLobbyList implements Differentiable {
-    private List<LightLobby> lobbies;
+    private transient ObservableList<LightLobby> lobbies;
     /**
      * Creates a LightLobbyList object.
      */
     public LightLobbyList() {
-        this.lobbies = new ArrayList<>();
+        this.lobbies = FXCollections.observableArrayList();
     }
     public LightLobbyList(List<LightLobby> lobbies){
-        this.lobbies = lobbies;
+        ObservableList<LightLobby> newList = FXCollections.observableArrayList();
+        newList.addAll(lobbies);
+        this.lobbies = newList;
     }
     /**
      * Returns the list of lobbies.
      * @return the list of lobbies
      */
-    public List<LightLobby> getLobbies() {
+    public synchronized List<LightLobby> getLobbies() {
         return lobbies;
     }
     /**
      * @param add the lobbies to add
      * @param rmv the lobbies to remove
      */
-    public void lobbiesDiff(List<LightLobby> add, List<LightLobby> rmv){
-        ArrayList<LightLobby> lobbyModify = new ArrayList<>(lobbies);
+    public synchronized void lobbiesDiff(List<LightLobby> add, List<LightLobby> rmv){
+        List<LightLobby> lobbyModify = FXCollections.observableList(lobbies);
         for(LightLobby lightLobby : rmv){
             lobbyModify.remove(lightLobby);
         }
         lobbyModify.addAll(add);
-        lobbies = lobbyModify;
     }
 
-    public void setLobbies(List<LightLobby> lobbies) {
-        this.lobbies = lobbies;
+    public synchronized void setLobbies(List<LightLobby> lobbies) {
+        ObservableList<LightLobby> newList = FXCollections.observableArrayList();
+        newList.addAll(lobbies);
+        this.lobbies = newList;
     }
 }
