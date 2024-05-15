a96ee609e6a7c6082590e0c8ca3c0b72ec9c3478
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-05-15 11:58:11 +0200

    feat: add rounded corners to cards and removed delay

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/Components/CardRelated/CardGUI.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/Components/CardRelated/CardGUI.java
@@ -3,23 +3,24 @@ package it.polimi.ingsw.view.GUI.Components.CardRelated;
 import it.polimi.ingsw.lightModel.LightCard;
 import it.polimi.ingsw.model.cardReleted.utilityEnums.CardFace;
 import it.polimi.ingsw.view.GUI.CardMuseumGUI;
+import it.polimi.ingsw.view.GUI.GUIConfigs;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
-import java.util.concurrent.atomic.AtomicBoolean;
+import javafx.scene.shape.Rectangle;
 
 public class CardGUI {
     private LightCard target;
     protected CardFace face;
     private final ImageView imageView = new ImageView();
     private Image image;
-    private double tx, ty;
-    private final Object lock = new Object();
-    private final AtomicBoolean farFromTarget = new AtomicBoolean(true);
-    private Thread translationThread;
-
 
     public CardGUI(LightCard target, CardFace face) {
+        Rectangle clip = new Rectangle(GUIConfigs.cardWidth, GUIConfigs.cardHeight);
+        clip.setArcWidth(40); // Set the horizontal radius of the arc
+        clip.setArcHeight(40); // Set the vertical radius of the arc
+
+        imageView.setClip(clip);
+
         this.target = target;
         this.face = face;
         this.update();
@@ -56,49 +57,9 @@ public class CardGUI {
         imageView.setY(y);
     }
 
-    public void setTargetTranslation(double x, double y) {
-        if(translationThread == null) {
-            translationThread = new Thread(() -> {
-                while(true) {
-                    if(target != null) {
-                        synchronized (lock) {
-                            while (!farFromTarget.get()) {
-                                try {
-                                    lock.wait();
-                                } catch (InterruptedException e) {
-                                    throw new RuntimeException(e);
-                                }
-                            }
-
-                            double dx = tx - imageView.getTranslateX();
-                            double dy = ty - imageView.getTranslateY();
-                            double dist = dx * dx + dy * dy;
-                            if (dist > 1) {
-                                imageView.setTranslateX(imageView.getTranslateX() + dx / 10 );
-                                imageView.setTranslateY(imageView.getTranslateY() + dy / 10 );
-                            } else {
-                                farFromTarget.set(false);
-                            }
-                        }
-
-                        try {
-                            Thread.sleep(10);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                    }
-                }
-            }
-            }
-            );
-            translationThread.start();
-        }
-        synchronized (lock) {
-            tx = x;
-            ty = y;
-            farFromTarget.set(true);
-            lock.notifyAll();
-        }
-
+    public void setTranslation(double x, double y) {
+        this.imageView.setTranslateX(x);
+        this.imageView.setTranslateY(y);
     }
 
     public ImageView getImageView() {
