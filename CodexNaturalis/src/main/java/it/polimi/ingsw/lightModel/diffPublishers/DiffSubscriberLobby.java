310396a6742f9f95bba28d7a211d8e9c2e2f5354
figinii <matteo5.figini@mail.polimi.it>
2024-05-18 12:57:36 +0200

    feat: remove clear method

--- CodexNaturalis/src/main/java/it/polimi/ingsw/lightModel/diffPublishers/DiffSubscriberLobby.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/lightModel/diffPublishers/DiffSubscriberLobby.java
@@ -6,4 +6,5 @@ import it.polimi.ingsw.lightModel.lightTableRelated.LightLobby;
 public interface DiffSubscriberLobby {
     void updateLobby(ModelDiffs<LightLobby> diff);
     String getNickname();
+    void gameStarted();
 }
