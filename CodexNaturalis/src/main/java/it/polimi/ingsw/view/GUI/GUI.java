487b8f44bba62bc25d613fcebc384e43a782319b
figinii <matteo5.figini@mail.polimi.it>
2024-05-21 09:19:10 +0200

    feat: transitionTo SecretObjective

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/GUI.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/GUI.java
@@ -67,10 +67,11 @@ public class GUI extends Application implements ActualView {
 
         //set stackRoot background and style
         //stackRoot.setStyle("-fx-background-color: #1e1f22;");
-        transitionTo(StateGUI.SERVER_CONNECTION);
+        //transitionTo(StateGUI.SERVER_CONNECTION);
+        //transitionTo(StateGUI.SELECT_OBJECTIVE);
         //transitionTo(StateGUI.JOIN_LOBBY);
         //transitionTo(StateGUI.LOBBY);
-        //transitionTo(StateGUI.PLACE_CARD);
+        transitionTo(StateGUI.PLACE_CARD);
     }
 
     private void setRoot(Node root){
