0d2c995f00c61492f4e5689da5481328c05fd96c
Teo <teo.lanzafme@mail.polimi.it>
2024-05-21 12:14:12 +0200

    feat: PostGameControllerGUI

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/Controllers/PostGameControllerGUI.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.view.GUI.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class PostGameControllerGUI {
+    @FXML
+    private Text winnersText;
+
+    @FXML
+    private void playAgain() {
+        // Logica per avviare una nuova partita
+    }
+
+    @FXML
+    private void goToMainMenu() {
+        // Logica per tornare al menu principale
+    }
+}
