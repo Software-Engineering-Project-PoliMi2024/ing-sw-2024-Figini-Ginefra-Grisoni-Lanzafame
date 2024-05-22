44ac25108a4b1ac85a24826ac14e3085a22e6fdd
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-05-22 13:58:10 +0200

    feat: add checking for conflicts

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cardReleted/pointMultiplyer/LCardPointMultiplier.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cardReleted/pointMultiplyer/LCardPointMultiplier.java
@@ -5,6 +5,10 @@ import it.polimi.ingsw.model.cardReleted.utilityEnums.CardFace;
 import it.polimi.ingsw.model.cardReleted.utilityEnums.Resource;
 import it.polimi.ingsw.model.playerReleted.Codex;
 import it.polimi.ingsw.model.playerReleted.Placement;
+import it.polimi.ingsw.model.playerReleted.Position;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class LCardPointMultiplier implements ObjectiveCardPointMultiplier {
     final private CardCorner corner;
@@ -31,22 +35,27 @@ public class LCardPointMultiplier implements ObjectiveCardPointMultiplier {
     @Override
     public int getMultiplier(Codex codex) {
         int multiplier = 0;
+        Set<Position> alreadySeen = new HashSet<>();
         for(Placement p : codex.getPlacementHistory()){
             if(p.card().getPermanentResources(CardFace.BACK).contains(singleResource) &&
                     codex.getPlacementAt(p.position()
                             .add(corner.getOffset()))!= null &&
+                    !alreadySeen.contains(p.position().add(corner.getOffset())) &&
                     codex.getPlacementAt(p.position()
                             .add(corner.getOffset())).card()
                             .getPermanentResources(CardFace.BACK).contains(doubleResource) &&
                     codex.getPlacementAt(p.position()
                             .add(corner.getOffset())
                             .add(0, 2*corner.getOffset().getY()))!= null &&
+                    !alreadySeen.contains(p.position().add(corner.getOffset()).add(0, 2*corner.getOffset().getY())) &&
                     codex.getPlacementAt(p.position()
                             .add(corner.getOffset())
                             .add(0, 2*corner.getOffset().getY())).card()
                             .getPermanentResources(CardFace.BACK).contains(doubleResource)
             ){
                 multiplier++;
+                alreadySeen.add(p.position().add(corner.getOffset()));
+                alreadySeen.add(p.position().add(corner.getOffset().add(0, 2*corner.getOffset().getY())));
             }
         }
         return multiplier;
