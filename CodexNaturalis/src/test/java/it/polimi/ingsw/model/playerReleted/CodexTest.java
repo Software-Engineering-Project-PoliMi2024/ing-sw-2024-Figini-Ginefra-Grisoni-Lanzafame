7e55e8606e75b1950ceaa8240bb5cda4611f3215
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-05-23 12:05:49 +0200

    test: add test for calculateConsequencesCollectables

--- CodexNaturalis/src/test/java/it/polimi/ingsw/model/playerReleted/CodexTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/model/playerReleted/CodexTest.java
@@ -1,7 +1,10 @@
 package it.polimi.ingsw.model.playerReleted;
 
-import it.polimi.ingsw.model.cardReleted.utilityEnums.SpecialCollectable;
+import it.polimi.ingsw.model.cardReleted.cards.ResourceCard;
+import it.polimi.ingsw.model.cardReleted.utilityEnums.*;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,9 +24,40 @@ class CodexTest {
     }
 
     @Test
-    void playCard() {
+    void calculateConsequences() {
         Codex codex = new Codex();
+        ResourceCard card = new ResourceCard(1, 1, Resource.FUNGI, 0, Map.of(
+                CardCorner.TR, Resource.ANIMAL,
+                CardCorner.TL, Resource.FUNGI,
+                CardCorner.BR, Resource.PLANT,
+                CardCorner.BL, Resource.INSECT
+        ));
 
+        ResourceCard card1 = new ResourceCard(1, 1, Resource.PLANT, 0, Map.of(
+                CardCorner.TR, Resource.ANIMAL,
+                CardCorner.TL, Resource.FUNGI,
+                CardCorner.BR, Resource.PLANT,
+                CardCorner.BL, Resource.INSECT
+        ));
+
+        codex.playCard(new Placement(new Position(0, 0), card, CardFace.FRONT));
+        codex.playCard(new Placement(new Position(1, 1), card1, CardFace.FRONT));
+        Map<Collectable, Integer> expected = Map.of(
+                Resource.ANIMAL, 1,
+                Resource.FUNGI, 3,
+                Resource.PLANT, 3,
+                Resource.INSECT, 2,
+                WritingMaterial.INKWELL, 0,
+                WritingMaterial.MANUSCRIPT, 0,
+                WritingMaterial.QUILL, 0
+        );
+
+        expected.forEach((collectable, integer) -> {
+            System.out.println(collectable + " " + integer);
+            assertEquals(integer, codex.getEarnedCollectables().get(collectable));
+        });
+
+        assertEquals(expected.size(), codex.getEarnedCollectables().size());
     }
 
 
