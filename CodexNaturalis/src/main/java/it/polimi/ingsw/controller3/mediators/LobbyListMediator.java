c4230631a471a4638ccf1b17a1c1b76bb28063f9
figinii <matteo5.figini@mail.polimi.it>
2024-05-23 23:28:26 +0200

    feat: methods to notify about creation and destruction of a Lobby

--- CodexNaturalis/src/main/java/it/polimi/ingsw/controller3/mediators/LobbyListMediator.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller3/mediators/LobbyListMediator.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.controller3.mediators;
 
 import it.polimi.ingsw.controller.LogsOnClient;
+import it.polimi.ingsw.controller3.LogsOnClientStatic;
 import it.polimi.ingsw.lightModel.LightModelUpdaterInterfaces.LightLobbyListUpdater;
 import it.polimi.ingsw.lightModel.diffs.ModelDiffs;
 import it.polimi.ingsw.lightModel.lightTableRelated.LightLobbyList;
@@ -9,6 +10,43 @@ import javax.xml.transform.Templates;
 
 
 public class LobbyListMediator extends Mediator<LightLobbyListUpdater, LightLobbyList> {
+    /**
+     * Notifies all the subscribers that a lobby has been added
+     * Adds the lobby to the lightLobbyList connected to the updater
+     * Sends the diff passed as parameter and logs the event
+     * @param creator the subscriber that created the lobby
+     * @param diff the diff of the lightLobbyList that adds the lobby
+     */
+    public void notifyNewLobby(String creator, ModelDiffs<LightLobbyList> diff){
+        subscribers.forEach((nickname, pair) -> {
+            try {
+                pair.first().updateLobbyList(diff);
+                if(!nickname.equals(creator))
+                    pair.second().log(LogsOnClientStatic.LOBBY_CREATED_OTHERS);
+                else
+                    pair.second().log(LogsOnClientStatic.LOBBY_CREATED_YOU);
+            } catch (Exception e) {
+                System.out.println("LobbyListMediator: error in notifying about the new lobby" + e.getMessage());
+            }
+        });
+    }
 
+    /**
+     * Notifies the subscriber that removed the lobby
+     * Removes the lobby from the lightLobbyList connected to the updater
+     * @param destroyer the subscriber that removed the lobby
+     * @param diff the diff that removes the lobby from the lightLobbyList on client
+     */
+    public void notifyLobbyRemoved(String destroyer, ModelDiffs<LightLobbyList> diff){
+        subscribers.forEach((nickname, pair) -> {
+            try {
+                pair.first().updateLobbyList(diff);
+                if(nickname.equals(destroyer))
+                    pair.second().log(LogsOnClientStatic.LOBBY_REMOVED_YOU);
+            } catch (Exception e) {
+                System.out.println("LobbyListMediator: error in notifying about the removed lobby" + e.getMessage());
+            }
+        });
+    }
 
 }
