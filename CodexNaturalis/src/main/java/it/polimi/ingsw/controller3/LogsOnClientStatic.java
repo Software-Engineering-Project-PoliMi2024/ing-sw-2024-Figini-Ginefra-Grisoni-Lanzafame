d8640b212c4491ba56e8319dba63e4befc0a3030
figinii <matteo5.figini@mail.polimi.it>
2024-05-23 23:24:42 +0200

    feat: class that contains logs on client that are now static string

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller3/LogsOnClientStatic.java
@@ -0,0 +1,69 @@
+package it.polimi.ingsw.controller3;
+
+import it.polimi.ingsw.Configs;
+
+public class LogsOnClientStatic {
+    //connectionForm related
+    public static String CONNECTION_SUCCESS = "Connected to the server successfully.";
+    public static String CONNECTION_ERROR = "Failed to connect to the server.";
+    public static String CONNECTION_LOST_CLIENT_SIDE = "Lost connection with the server.";
+
+    //login related
+    public static String NAME_TAKEN = "The chosen nickname is already in use.";
+    public static String SERVER_JOINED = "Joined the server.";
+    public static String NOT_VALID_NICKNAME = "The chosen nickname is not valid.";
+
+    //lobbyList related
+    public static String LOBBY_NONEXISTENT = "The queried lobby does not exist.";
+    public static String LOBBY_NAME_TAKEN = "The specified lobby name is already taken.";
+    public static String LOBBY_IS_FULL = "The lobby is currently full.";
+    public static String LOBBY_CREATED_YOU = "New Lobby creation successful.";
+    public static String LOBBY_CREATED_OTHERS = "Added a new lobby.";
+    public static String LOBBY_REMOVED_YOU = "Lobby removal successful.";
+
+    //lobby related
+    public static String LOBBY_JOINED = "Successfully joined the lobby.";
+    public static String LOBBY_LEFT = "Left the lobby.";
+    public static String PLAYER_JOIN_LOBBY = " joined the lobby";
+
+    //joinGame related
+    public static String GAME_CREATED = "Lobby is full, the game is starting.";
+    public static String NEW_GAME_JOINED = "Joined a new game.";
+    public static String MID_GAME_JOINED = "Rejoined a previous game.";
+
+    //gameSetup related  = startCard face and SecretObjective option selection)
+    public static String DECK_SHUFFLE = "The Resource and Gold card decks have been shuffled.";
+    public static String YOU_PLACE_STARTCARD = "Successfully placed your Start Card.";
+    public static String YOU_CHOSE = "Successfully chose your Secret Objective.";
+    public static String PLAYER_PLACE_STARTCARD = " placed their Start Card.";
+    public static String PLAYER_CHOSE = " chose their Secret Objective.";
+    public static String EVERYONE_PLACED_STARTCARD = "All players have placed their Start Cards.";
+    public static String WAIT_SECRET_OBJECTIVE = "Waiting for other players to choose their Secret Objectives.";
+    public static String EVERYONE_CHOSE = "All players have chosen their Secret Objectives.";
+
+    //gameCards related  = card place or draw)
+    public static String CARD_PLACED = "Card successfully placed.";
+    public static String CARD_DRAWN = "Card successfully drawn.";
+
+    public static String PLAYER_PLACED = " placed a card.";
+    public static String YOU_PLACED = "Successfully placed a card in your Codex.";
+    public static String PLAYER_DRAW = " drew a card.";
+    public static String YOU_DRAW = "Successfully drew a card to your Hand.";
+
+
+    //gameTurn related
+    public static String YOUR_TURN = "It's now your turn to play.";
+    public static String PLAYER_TURN = "'s turn";
+    public static String WAIT_STARTCARD = "Waiting for other players to place their Start Cards.";
+    public static String LAST_TURN = "The final round of turns has begun.";
+    public static String GAME_END = "The game has ended.";
+
+    //gameParty related
+    public static String PLAYER_REJOINED = " rejoined the game";
+    public static String PLAYER_GAME_LEFT = " left the game";
+    public static String LAST_PLAYER = "You are the last player in the game";
+
+    //gameCountDown related
+    public static String COUNTDOWN_START = "A CountDown of " +Configs.timerDurationSeconds + " seconds is starting now";
+    public static String COUNTDOWN_INTERRUPTED = "The CountDown has been stopped";
+}
