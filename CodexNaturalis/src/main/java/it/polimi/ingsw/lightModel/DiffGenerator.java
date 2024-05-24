cda5f039cdf04dc45e5134f6f9114dc4ac9f84ef
figinii <matteo5.figini@mail.polimi.it>
2024-05-24 12:48:55 +0200

    feat: methods to generate the diff for the lobby

--- CodexNaturalis/src/main/java/it/polimi/ingsw/lightModel/DiffGenerator.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/lightModel/DiffGenerator.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.lightModel;
 
 
+import it.polimi.ingsw.lightModel.diffs.lobby_lobbyList.LobbyDiffEdit;
+import it.polimi.ingsw.lightModel.diffs.lobby_lobbyList.LobbyDiffEditLogin;
 import it.polimi.ingsw.lightModel.diffs.lobby_lobbyList.LobbyListDiff;
 import it.polimi.ingsw.lightModel.diffs.lobby_lobbyList.LobbyListDiffEdit;
 import it.polimi.ingsw.lightModel.lightTableRelated.LightLobby;
@@ -21,5 +23,17 @@ public class DiffGenerator {
 
     public static LobbyListDiff lobbyListHistory(List<Lobby> lobbyHistory){
         return new LobbyListDiffEdit(new ArrayList<LightLobby>(Lightifier.lightify(lobbyHistory)), new ArrayList<LightLobby>());
+    }
+
+    public static LobbyDiffEditLogin diffJoinLobby(Lobby lobbyJoined){
+        return new LobbyDiffEditLogin(lobbyJoined.getLobbyPlayerList(), new ArrayList<>(), lobbyJoined.getLobbyName(), lobbyJoined.getNumberOfMaxPlayer());
+    }
+
+    public static LobbyDiffEdit diffAddUserToLobby(String nickname){
+        return new LobbyDiffEdit(List.of(nickname), new ArrayList<>());
+    }
+
+    public static LobbyDiffEdit diffRemoveUserFromLobby(String nickname){
+        return new LobbyDiffEdit(new ArrayList<>(), List.of(nickname));
     }
 }
