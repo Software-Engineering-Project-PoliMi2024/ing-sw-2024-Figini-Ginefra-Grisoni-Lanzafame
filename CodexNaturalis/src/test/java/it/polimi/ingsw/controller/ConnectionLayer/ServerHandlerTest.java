90cf08d54ef8c23d2994edad2753b4e0d814d07b
dedepivot <samuele.grisoni@gmail.com>
2024-05-25 19:10:15 +0200

    chore: removed legacy code

--- CodexNaturalis/src/test/java/it/polimi/ingsw/controller/ConnectionLayer/ServerHandlerTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/controller/ConnectionLayer/ServerHandlerTest.java
@@ -1,24 +1,18 @@
 package it.polimi.ingsw.controller.ConnectionLayer;
 
-import it.polimi.ingsw.Server;
 import it.polimi.ingsw.connectionLayer.Socket.ServerHandler;
 import it.polimi.ingsw.connectionLayer.Socket.ServerMsg.*;
 import it.polimi.ingsw.connectionLayer.VirtualLayer.VirtualController;
 import it.polimi.ingsw.connectionLayer.VirtualSocket.VirtualControllerSocket;
 import it.polimi.ingsw.view.ViewInterface;
 import it.polimi.ingsw.view.ViewState;
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
-import java.lang.reflect.Method;
 import java.net.Socket;
 import java.util.LinkedList;
 import java.util.Random;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.*;
 
 public class ServerHandlerTest {
@@ -34,26 +28,26 @@ public class ServerHandlerTest {
         int numberOfMessages = random.nextInt(1, 10);
         for(int i = 0; i < numberOfMessages; i++) {
             ServerMsg msg = new LogMsg("hi");
-            msg.setIndex(i+2); //keep in mind that index 0 is used during the setup
+            msg.setIndex(i+1); //keep in mind that index 0 is the expected index
             toServerHandler.writeObject(msg);
             toServerHandler.flush();
         }
         Thread.sleep(30); //Allow some time for the serverHandler to process the messages
-        Assertions.assertEquals(serverHandler.getRecivedMsgs().size(), numberOfMessages);
+        Assertions.assertEquals(serverHandler.getReceivedMsg().size(), numberOfMessages);
 
         //Check if the messages are stored in the right order
-        LinkedList<ServerMsg> messageList = new LinkedList<>(serverHandler.getRecivedMsgs());
+        LinkedList<ServerMsg> messageList = new LinkedList<>(serverHandler.getReceivedMsg());
         for(int i = 0; i < numberOfMessages; i++) {
-            Assertions.assertEquals(messageList.get(i).getIndex(), i+2);
+            Assertions.assertEquals(messageList.get(i).getIndex(), i+1);
         }
 
         //Check what happens when the expected message is received after some out of order messages
         ServerMsg expectedMsg = new LogMsg("hi");
-        expectedMsg.setIndex(1);
+        expectedMsg.setIndex(0);
         toServerHandler.writeObject(expectedMsg);
         toServerHandler.flush();
         Thread.sleep(30); //Allow some time for the serverHandler to all the messages in the queue
-        Assertions.assertEquals(serverHandler.getRecivedMsgs().size(), 0);
+        Assertions.assertEquals(serverHandler.getReceivedMsg().size(), 0);
     }
 
     @Test
@@ -67,7 +61,7 @@ public class ServerHandlerTest {
         int numberOfMessages = random.nextInt(1, 10);
         for(int i = 0; i < numberOfMessages; i++) {
             ServerMsg continueMsg = new LogErrMsg("hi");
-            continueMsg.setIndex(i+2);
+            continueMsg.setIndex(i+1);
             toServerHandler.writeObject(continueMsg);
             toServerHandler.flush();
 
@@ -77,14 +71,14 @@ public class ServerHandlerTest {
             toServerHandler.flush();
         }
         ServerMsg expectedMsg = new LogMsg("hi");
-        expectedMsg.setIndex(1);
+        expectedMsg.setIndex(0);
         toServerHandler.writeObject(expectedMsg);
         toServerHandler.flush();
         Thread.sleep(30); //Allow some time for the serverHandler to all the messages in the queue
 
         //The number of messages in the queue should be equal to the number of messages that are not part of the continue window
         //The number of messages not present in the continue window is equal to numberOfMessages created in the loop above
-        Assertions.assertEquals(serverHandler.getRecivedMsgs().size(), numberOfMessages);
+        Assertions.assertEquals(serverHandler.getReceivedMsg().size(), numberOfMessages);
     }
 
     @Test
@@ -96,7 +90,6 @@ public class ServerHandlerTest {
 
         // Send a LogErrMsg to the ServerHandler
         ServerMsg logErrMsg = new LogErrMsg("Error message");
-        logErrMsg.setIndex(1);
         toServerHandler.writeObject(logErrMsg);
         toServerHandler.flush();
         Thread.sleep(10);
@@ -114,7 +107,6 @@ public class ServerHandlerTest {
 
         // Send a LogGameMsg to the ServerHandler
         ServerMsg logGameMsg = new LogGameMsg("Game message");
-        logGameMsg.setIndex(1);
         toServerHandler.writeObject(logGameMsg);
         toServerHandler.flush();
         Thread.sleep(10);
@@ -132,7 +124,6 @@ public class ServerHandlerTest {
 
         // Send a LogMsg to the ServerHandler
         ServerMsg logMsg = new LogMsg("Message");
-        logMsg.setIndex(1);
         toServerHandler.writeObject(logMsg);
         toServerHandler.flush();
         Thread.sleep(10);
@@ -150,7 +141,6 @@ public class ServerHandlerTest {
 
         // Send a LogOthersMsg to the ServerHandler
         ServerMsg logOthersMsg = new LogOthersMsg("Other message");
-        logOthersMsg.setIndex(1);
         toServerHandler.writeObject(logOthersMsg);
         toServerHandler.flush();
         Thread.sleep(10);
@@ -168,7 +158,6 @@ public class ServerHandlerTest {
 
         // Send a SetFinalRankingMsg to the ServerHandler
         ServerMsg setFinalRankingMsg = new SetFinalRankingMsg(new String[]{"Player1", "Player2"}, new int[]{1, 2});
-        setFinalRankingMsg.setIndex(1);
         toServerHandler.writeObject(setFinalRankingMsg);
         toServerHandler.flush();
         Thread.sleep(10);
@@ -186,7 +175,6 @@ public class ServerHandlerTest {
 
         // Send a UpdateGameMsg to the ServerHandler
         ServerMsg updateGameMsg = new UpdateGameMsg(null);
-        updateGameMsg.setIndex(1);
         toServerHandler.writeObject(updateGameMsg);
         toServerHandler.flush();
         Thread.sleep(10);
@@ -204,7 +192,6 @@ public class ServerHandlerTest {
 
         // Send a UpdateLobbyListMsg to the ServerHandler
         ServerMsg updateLobbyListMsg = new UpdateLobbyListMsg(null);
-        updateLobbyListMsg.setIndex(1);
         toServerHandler.writeObject(updateLobbyListMsg);
         toServerHandler.flush();
         Thread.sleep(10);
@@ -222,7 +209,6 @@ public class ServerHandlerTest {
 
         // Send a UpdateLobbyMsg to the ServerHandler
         ServerMsg updateLobbyMsg = new UpdateLobbyMsg(null);
-        updateLobbyMsg.setIndex(1);
         toServerHandler.writeObject(updateLobbyMsg);
         toServerHandler.flush();
         Thread.sleep(10);
@@ -233,7 +219,7 @@ public class ServerHandlerTest {
 
     @Test
     public void testTransitionToMsgInvocation() throws Exception {
-        //todo check why is slow. Probably because is something to do with the fact that a TransitionToMsg is used in the Setup
+        //todo check why this test is so slow.
         ServerHandlerTestSetup setup = setupServerHandler();
 
         ObjectOutputStream toServerHandler = setup.toServerHandler();
@@ -241,7 +227,6 @@ public class ServerHandlerTest {
 
         // Send a TransitionToMsg to the ServerHandler
         ServerMsg transitionToMsg = new TransitionToMsg(ViewState.GAME_ENDING);
-        transitionToMsg.setIndex(1);
         toServerHandler.writeObject(transitionToMsg);
         toServerHandler.flush();
         Thread.sleep(10);
@@ -256,12 +241,7 @@ public class ServerHandlerTest {
         PipedOutputStream outputPipe = new PipedOutputStream();
         inputPipe.connect(outputPipe);
 
-        //For some reason, the ObjectInputStream's constructor blocks the code flow until there is some inputData to read
-        //This is handy in some scenario, e.g. checking if the right protocol is selected, but here I need to simulate a successful connection
         ObjectOutputStream toServerHandler = new ObjectOutputStream(outputPipe);
-        toServerHandler.writeObject(new TransitionToMsg(ViewState.LOGIN_FORM));
-        toServerHandler.flush();
-
         ByteArrayOutputStream fromServerHandler = new ByteArrayOutputStream();
         Socket mockServer = mock(Socket.class);
         when(mockServer.getOutputStream()).thenReturn(fromServerHandler);
