b9f9868ca5bff3d6bc5983076e3a94e857d1a789
dedepivot <samuele.grisoni@gmail.com>
2024-05-25 18:22:21 +0200

    test: add continueMessageWindowTest, logErrTest

--- CodexNaturalis/src/test/java/it/polimi/ingsw/controller/ConnectionLayer/ServerHandlerTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/controller/ConnectionLayer/ServerHandlerTest.java
@@ -2,31 +2,34 @@ package it.polimi.ingsw.controller.ConnectionLayer;
 
 import it.polimi.ingsw.Server;
 import it.polimi.ingsw.connectionLayer.Socket.ServerHandler;
-import it.polimi.ingsw.connectionLayer.Socket.ServerMsg.LogErrMsg;
-import it.polimi.ingsw.connectionLayer.Socket.ServerMsg.LogMsg;
-import it.polimi.ingsw.connectionLayer.Socket.ServerMsg.ServerMsg;
-import it.polimi.ingsw.connectionLayer.Socket.ServerMsg.TransitionToMsg;
+import it.polimi.ingsw.connectionLayer.Socket.ServerMsg.*;
 import it.polimi.ingsw.connectionLayer.VirtualLayer.VirtualController;
 import it.polimi.ingsw.connectionLayer.VirtualSocket.VirtualControllerSocket;
 import it.polimi.ingsw.view.ViewInterface;
 import it.polimi.ingsw.view.ViewState;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
+import java.lang.reflect.Method;
 import java.net.Socket;
 import java.util.LinkedList;
 import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.*;
 
 public class ServerHandlerTest {
     @Test
-    public void testOutOfOrderMessage() throws IOException, InterruptedException {
+    public void testOutOfOrderMessage() throws IOException, InterruptedException, NoSuchMethodException {
         ServerHandlerTestSetup setup = setupServerHandler();
+
         ServerHandler serverHandler = setup.serverHandler();
         ObjectOutputStream toServerHandler = setup.toServerHandler();
+
+        //Check if all out of order messages are stored in the serverHandler
         Random random = new Random();
         int numberOfMessages = random.nextInt(1, 10);
         for(int i = 0; i < numberOfMessages; i++) {
@@ -35,15 +38,72 @@ public class ServerHandlerTest {
             toServerHandler.writeObject(msg);
             toServerHandler.flush();
         }
-        Thread.sleep(100); //Allow some time for the serverHandler to process the messages
-        //Check if all messages are stored in the serverHandler
-        assertEquals(serverHandler.getRecivedMsgs().size(), numberOfMessages);
+        Thread.sleep(30); //Allow some time for the serverHandler to process the messages
+        Assertions.assertEquals(serverHandler.getRecivedMsgs().size(), numberOfMessages);
 
         //Check if the messages are stored in the right order
         LinkedList<ServerMsg> messageList = new LinkedList<>(serverHandler.getRecivedMsgs());
         for(int i = 0; i < numberOfMessages; i++) {
-            assertEquals(messageList.get(i).getIndex(), i+2);
+            Assertions.assertEquals(messageList.get(i).getIndex(), i+2);
         }
+
+        //Check what happens when the expected message is received after some out of order messages
+        ServerMsg expectedMsg = new LogMsg("hi");
+        expectedMsg.setIndex(1);
+        toServerHandler.writeObject(expectedMsg);
+        toServerHandler.flush();
+        Thread.sleep(30); //Allow some time for the serverHandler to all the messages in the queue
+        Assertions.assertEquals(serverHandler.getRecivedMsgs().size(), 0);
+    }
+
+    @Test
+    public void testContinueMessagesWindow() throws IOException, InterruptedException {
+        ServerHandlerTestSetup setup = setupServerHandler();
+
+        ServerHandler serverHandler = setup.serverHandler();
+        ObjectOutputStream toServerHandler = setup.toServerHandler();
+
+        Random random = new Random();
+        int numberOfMessages = random.nextInt(1, 10);
+        for(int i = 0; i < numberOfMessages; i++) {
+            ServerMsg continueMsg = new LogErrMsg("hi");
+            continueMsg.setIndex(i+2);
+            toServerHandler.writeObject(continueMsg);
+            toServerHandler.flush();
+
+            ServerMsg notContinueMsg = new LogOthersMsg("hi");
+            notContinueMsg.setIndex((int) Math.pow(2, i+4)); //the non ContinueMsg will start with index  of 16
+            toServerHandler.writeObject(notContinueMsg);
+            toServerHandler.flush();
+        }
+        ServerMsg expectedMsg = new LogMsg("hi");
+        expectedMsg.setIndex(1);
+        toServerHandler.writeObject(expectedMsg);
+        toServerHandler.flush();
+        Thread.sleep(30); //Allow some time for the serverHandler to all the messages in the queue
+
+        //The number of messages in the queue should be equal to the number of messages that are not part of the continue window
+        //The number of messages not present in the continue window is equal to numberOfMessages created in the loop above
+        Assertions.assertEquals(serverHandler.getRecivedMsgs().size(), numberOfMessages);
+    }
+
+    @Test
+    public void testLogErrInvocation() throws Exception {
+        ServerHandlerTestSetup setup = setupServerHandler();
+
+        ServerHandler serverHandler = setup.serverHandler();
+        ObjectOutputStream toServerHandler = setup.toServerHandler();
+        ViewInterface mockView = setup.mockView();
+
+        // Send a LogErrMsg to the ServerHandler
+        ServerMsg logErrMsg = new LogErrMsg("Error message");
+        logErrMsg.setIndex(1);
+        toServerHandler.writeObject(logErrMsg);
+        toServerHandler.flush();
+        Thread.sleep(10);
+
+        // Verify if logErr was called on the mockView
+        verify(mockView, times(1)).logErr(anyString());
     }
 
     private ServerHandlerTestSetup setupServerHandler() throws IOException {
@@ -52,7 +112,7 @@ public class ServerHandlerTest {
         inputPipe.connect(outputPipe);
 
         //For some reason, the ObjectInputStream's constructor blocks the code flow until there is some inputData to read
-        //This is handy in some scenario, e.g. checking if the right protocol is selected, but here we need to simulate a succefully connection
+        //This is handy in some scenario, e.g. checking if the right protocol is selected, but here I need to simulate a successful connection
         ObjectOutputStream toServerHandler = new ObjectOutputStream(outputPipe);
         toServerHandler.writeObject(new TransitionToMsg(ViewState.LOGIN_FORM));
         toServerHandler.flush();
@@ -73,8 +133,8 @@ public class ServerHandlerTest {
         serverHandler.setView(mockView);
         serverHandler.setOwner(mockController);
 
-        return new ServerHandlerTestSetup(serverHandler, toServerHandler);
+        return new ServerHandlerTestSetup(serverHandler, toServerHandler, mockView);
     }
 
-    private record ServerHandlerTestSetup(ServerHandler serverHandler, ObjectOutputStream toServerHandler) {}
+    private record ServerHandlerTestSetup(ServerHandler serverHandler, ObjectOutputStream toServerHandler, ViewInterface mockView) {}
 }
