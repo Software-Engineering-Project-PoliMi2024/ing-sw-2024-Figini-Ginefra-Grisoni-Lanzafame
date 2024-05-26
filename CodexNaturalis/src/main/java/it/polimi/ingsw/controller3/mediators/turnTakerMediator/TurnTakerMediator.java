a906d3607617a90fd7ba0d592229cb97cb8efeac
figinii <matteo5.figini@mail.polimi.it>
2024-05-26 02:31:22 +0200

    feat: add TakeTurnMediator and lock to GameParty

--- CodexNaturalis/src/main/java/it/polimi/ingsw/controller3/mediators/turnTakerMediator/TurnTakerMediator.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller3/mediators/turnTakerMediator/TurnTakerMediator.java
@@ -1,13 +1,42 @@
 package it.polimi.ingsw.controller3.mediators.turnTakerMediator;
 
+import java.util.List;
+
 public class TurnTakerMediator extends GenericMediator<TurnTaker>{
+    /**
+     * This method is used to subscribe a new player to the mediator
+     * subscribing a player means that he will be able to be notified when it is his turn
+     * and will be considered active
+     * @param nickname the nickname of the player that wants to subscribe
+     * @param subscriber the subscriber that wants to subscribe
+     */
     public synchronized void subscribe(String nickname, TurnTaker subscriber){
         super.subscribe(nickname, subscriber);
     }
+
+    /**
+     * This method is used to unsubscribe a player from the mediator
+     * thus making him inactive and unable to receive notifications
+     * @param nickname the nickname of the player that wants to unsubscribe
+     */
     public synchronized void unsubscribe(String nickname){
         super.unsubscribe(nickname);
     }
-    public synchronized void takeTurn(String nickname){
+
+    /**
+     * This method is used to notify a player that it is his turn
+     * by calling the takeTurn method of the subscriber
+     * @param nickname the nickname of the player that is being notified
+     */
+    public synchronized void notifyTurn(String nickname){
         subscribers.get(nickname).takeTurn();
+    }
+
+    /**
+     * This method is used to get the list of active player's nickname
+     * @return the list of active player's nickname
+     */
+    public synchronized List<String> getActivePlayers(){
+        return subscribers.keySet().stream().toList();
     }
 }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/tableReleted/GameParty.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/tableReleted/GameParty.java
@@ -1,49 +1,53 @@
 package it.polimi.ingsw.model.tableReleted;
 
+import it.polimi.ingsw.controller3.mediators.turnTakerMediator.TurnTaker;
+import it.polimi.ingsw.controller3.mediators.turnTakerMediator.TurnTakerMediator;
 import it.polimi.ingsw.model.playerReleted.User;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
 
 public class GameParty implements Serializable {
     final private List<User> playerList; //the player that were in the lobby pre game
-    final private List<User> activePlayers; //the player that are currently playing in the game
     private int currentPlayerIndex;
+    private final TurnTakerMediator activeTurnTakerMediator = new TurnTakerMediator();
+    private final ReentrantLock gamePartyLock = new ReentrantLock(true);
 
     /**
      * The constructor of the class
      * playerList is shuffled to create a random order of the players
-     * all players are considered active at the creation of the gameParty
+     * all players are considered inactive at the creation of the gameParty
      * @param playerNames list of all players nicks' who can join this game
      */
     public GameParty(List<String> playerNames) {
         ArrayList<String> players = new ArrayList<>(playerNames);
         Collections.shuffle(players);
         playerList = players.stream().map(User::new).collect(Collectors.toList());
-        currentPlayerIndex = -1;
-        activePlayers = new ArrayList<>(playerList);
+        currentPlayerIndex = 0;
     }
 
     /**
-     * This method advances the game to the next player in the rotation sequence.
-     * if there is no currentPlayer, it creates it by launching the chooseStartingOrder method
-     * @throws IllegalCallerException if the game is empty.
+     * This method is used to set the player index in the game
+     * @param index the index of the player that is playing
      */
-    public User nextPlayer() throws IllegalCallerException {
-        if(playerList.isEmpty()){
-            throw new IllegalCallerException("The game is empty, there is no next player");
-        } else {
-            currentPlayerIndex = (currentPlayerIndex + 1) % playerList.size();
-            return playerList.get(currentPlayerIndex);
-        }
+    public void setPlayerIndex(int index){
+        currentPlayerIndex = index;
     }
+
+    /**
+     * This method is used to get the index of the next player that is supposed to play
+     * not considering the possible disconnection that may have occurred
+     * @return the index of the next player
+     */
+    public int getNextPlayerIndex() {
+        return (currentPlayerIndex % playerList.size());
+    }
+
     /** @return list of the players in this match*/
     public List<User> getUsersList() {
-        return new LinkedList<>(playerList);
+        return playerList;
     }
 
     /***
@@ -61,6 +65,58 @@ public class GameParty implements Serializable {
         playerList.remove(user);
     }
 
+    /**
+     * subscribe a turnTaker to the activeTurnTakerMediator in order to receive notification
+     * on when it is their turn to play
+     * @param nickname the nickname of the player that is subscribing
+     * @param turnTaker the turnTaker that is subscribing
+     */
+    public void subscribe(String nickname, TurnTaker turnTaker){
+        activeTurnTakerMediator.subscribe(nickname, turnTaker);
+    }
+
+    /**
+     * unsubscribe a player from the activeTurnTakerMediator
+     * removing them from the list of players that will receive notifications
+     * @param nickname the nickname of the player that is unsubscribing
+     */
+    public void unsubscribe(String nickname){
+        activeTurnTakerMediator.unsubscribe(nickname);
+    }
+
+    /**
+     * notify a player that it is their turn to play by
+     * calling the takeTurnMediator method of the subscriber
+     * @param nickname the nickname of the player that is being notified
+     */
+    public void notifyTurn(String nickname){
+        activeTurnTakerMediator.notifyTurn(nickname);
+    }
+
+    /**
+     * get the list of active players
+     * @return the list of active players
+     */
+    public List<String> getActivePlayers(){
+        return activeTurnTakerMediator.getActivePlayers();
+    }
+
+    /**
+     * acquires the lock on the gamePartyLock
+     */
+    public void lock(){
+        gamePartyLock.lock();
+    }
+
+    /**
+     * releases the lock on the gamePartyLock
+     */
+    public void unlock(){
+        gamePartyLock.unlock();
+    }
+
+
+//TODO to remove
     public User getFirstPlayerInOrder() {
         return new User(playerList.getFirst());
     }
@@ -68,15 +124,18 @@ public class GameParty implements Serializable {
     public User getCurrentPlayer() {
         return playerList.get(currentPlayerIndex);
     }
-    public List<String> getActivePlayers() {
-        return activePlayers.stream().map(User::getNickname).toList();
-    }
 
-    public void addActivePlayer(User user){
-        activePlayers.add(user);
-    }
-
-    public void removeActivePlayer(User user){
-        activePlayers.remove(user);
+    /**
+     * This method advances the game to the next player in the rotation sequence.
+     * if there is no currentPlayer, it creates it by launching the chooseStartingOrder method
+     * @throws IllegalCallerException if the game is empty.
+     */
+    public User nextPlayer() {
+        if(playerList.isEmpty()){
+            throw new IllegalCallerException("The game is empty, there is no next player");
+        } else {
+            currentPlayerIndex = getNextPlayerIndex();
+            return playerList.get(currentPlayerIndex);
+        }
     }
 }
