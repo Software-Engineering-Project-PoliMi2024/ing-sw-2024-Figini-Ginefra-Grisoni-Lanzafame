2f1c2e400a38ed15c4fd306b2b708cc0bc03c282
figinii <matteo5.figini@mail.polimi.it>
2024-05-26 21:47:54 +0200

    feat: add comment

--- CodexNaturalis/src/main/java/it/polimi/ingsw/lightModel/LightModelUpdaterInterfaces/LightGameUpdater.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/lightModel/LightModelUpdaterInterfaces/LightGameUpdater.java
@@ -4,6 +4,18 @@ import it.polimi.ingsw.lightModel.diffs.ModelDiffs;
 import it.polimi.ingsw.lightModel.lightTableRelated.LightGame;
 
 public interface LightGameUpdater extends Updater{
+    /**
+     * Update the game with a diff
+     * @param diff the diff to apply
+     * @throws Exception if the connection between the server and the client fails
+     */
     void updateGame(ModelDiffs<LightGame> diff) throws Exception;
+
+    /**
+     * Set the final ranking of the game
+     * @param nicks the nicknames of the players
+     * @param points the points of the players
+     * @throws Exception if the connection between the server and the client fails
+     */
     void setFinalRanking(String[] nicks, int[] points) throws Exception;
 }
