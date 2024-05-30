b2551d46678e2357da9818fa05c724d9f9f2519d
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-05-30 23:27:00 +0200

    chore: update the interfaces

--- CodexNaturalis/src/main/java/it/polimi/ingsw/controller4/Interfaces/ReceptionControllerInterface.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller4/Interfaces/ReceptionControllerInterface.java
@@ -3,7 +3,7 @@ package it.polimi.ingsw.controller4.Interfaces;
 import java.io.Serializable;
 import java.rmi.Remote;
 
-public interface ReceptionControllerInterface extends Serializable, Remote {
+public interface ReceptionControllerInterface extends Serializable {
     void login(String nickname);
     void createLobby(String nickname, String gameName, int maxPlayerCount);
     void joinLobby(String nickname, String lobbyName);
