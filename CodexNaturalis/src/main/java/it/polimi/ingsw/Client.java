17e38d9ae11fc42805aabd272feab362220ab4f9
dedepivot <samuele.grisoni@mail.polimi.it>
2024-05-30 16:40:54 +0200

    feat: add to Client the ability to create a Data Folder

--- CodexNaturalis/src/main/java/it/polimi/ingsw/Client.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Client.java
@@ -4,10 +4,12 @@ import it.polimi.ingsw.connectionLayer.VirtualLayer.VirtualController;
 import it.polimi.ingsw.view.GUI.GUI;
 import it.polimi.ingsw.view.TUI.TUI;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
+        checkOrCreateDataFolder();
         VirtualController controller;
         System.out.println("Hi there 👋!");
 
@@ -32,6 +34,23 @@ public class Client {
         } else{
             System.out.println("You chose the graphical interface!");
             new GUI().run();
+        }
+    }
+
+    private static void checkOrCreateDataFolder() {
+        File dataFolder = new File(OSRelated.dataFolderPath);
+        if (!dataFolder.exists()) {
+            dataFolder.mkdirs();
+        }
+        // Create Cards folder
+        File cardsFolder = new File(OSRelated.cardFolderDataPath);
+        if (!cardsFolder.exists()) {
+            cardsFolder.mkdirs();
+        }
+        // Create gameSaved folder
+        File gameSavedFolder = new File(OSRelated.gameDataFolderPath);
+        if (!gameSavedFolder.exists()) {
+            gameSavedFolder.mkdirs();
         }
     }
 }
