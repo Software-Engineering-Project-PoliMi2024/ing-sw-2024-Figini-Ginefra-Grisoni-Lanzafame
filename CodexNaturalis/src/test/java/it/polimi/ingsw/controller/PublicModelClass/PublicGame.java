f7d7b23e207867f96de0ead8db44fa30b9b7f75f
figinii <matteo5.figini@mail.polimi.it>
2024-06-17 02:09:07 +0200

    feat: add method to access lastTurnCounter

--- CodexNaturalis/src/test/java/it/polimi/ingsw/controller/PublicModelClass/PublicGame.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/controller/PublicModelClass/PublicGame.java
@@ -1,23 +1,23 @@
 package it.polimi.ingsw.controller.PublicModelClass;
 
 import it.polimi.ingsw.model.cardReleted.cards.ObjectiveCard;
+import it.polimi.ingsw.model.playerReleted.User;
 import it.polimi.ingsw.model.tableReleted.Game;
+import it.polimi.ingsw.model.tableReleted.GameParty;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.List;
 
-public class PublicGame {
-    public final Game game;
-
-    public PublicGame(Game game){
-        this.game = game;
-    }
+public record PublicGame(Game game) {
 
     /**
      * add a given objective card as the first public objective in Game
+     *
      * @param obj the objectiveCard to add to the Game public objectives
      */
-    public void setPublicObj(ObjectiveCard obj){
+    public void setPublicObj(ObjectiveCard obj) {
         try {
             Field commonObjectiveField = game.getClass().getDeclaredField("commonObjective");
             commonObjectiveField.setAccessible(true);
@@ -26,6 +26,17 @@ public class PublicGame {
             objectiveCards.addFirst(obj);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public Integer getLastTurnCounter(){
+        try{
+            Field lastTurnCounterField = game.getClass().getDeclaredField("lastTurnsCounter");
+            lastTurnCounterField.setAccessible(true);
+            return  (int) lastTurnCounterField.get(game);
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
         }
     }
 }
