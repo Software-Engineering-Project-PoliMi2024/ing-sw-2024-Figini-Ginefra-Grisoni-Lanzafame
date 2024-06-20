377832f9fd534231aa837564775689855ae8001c
dedepivot <samuele.grisoni@gmail.com>
2024-06-20 12:39:02 +0200

    refactor: change the render disposition in sendMessageContainer

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/Components/ChatDisplay.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/Components/ChatDisplay.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.view.GUI.Components;
 
 import it.polimi.ingsw.model.playerReleted.ChatMessage;
 import it.polimi.ingsw.view.GUI.Components.Utils.PopUp;
+import it.polimi.ingsw.view.GUI.GUI;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
@@ -15,6 +16,10 @@ public class ChatDisplay {
     private PopUp chatPopUp;
     private final ListView<ChatMessage> messages = new ListView<>();
 
+    private ChoiceBox<String> receiverChoice;
+    private Button sendButton;
+    private TextField sendMessageField;
+
     public ChatDisplay(AnchorPane parent) {
         chatPopUp = new PopUp(parent);
         messages.setCellFactory(param -> listViewChatMessageCellFactory());
@@ -26,27 +31,22 @@ public class ChatDisplay {
         AnchorPane.setLeftAnchor(popUpFiller, 0.0);
         AnchorPane.setRightAnchor(popUpFiller, 0.0);
 
+        //Create a container for received messages which will contain the ListView
         HBox receivedMessagesContainer = new HBox();
         receivedMessagesContainer.setAlignment(Pos.CENTER);
         receivedMessagesContainer.getChildren().add(messages);
         VBox.setVgrow(receivedMessagesContainer, Priority.ALWAYS);
 
-        HBox sendMessageContainer = new HBox();
+        //Create a container for sending messages which will contain the TextField, Button and the ChoiceBox
+        VBox sendMessageContainer = new VBox();
         sendMessageContainer.setSpacing(20.0);
         sendMessageContainer.setAlignment(Pos.CENTER);
-        TextField sendMessageField = new TextField();
-        sendMessageField.setPromptText("Write a message...");
-        sendMessageContainer.getChildren().add(sendMessageField);
-
-        VBox sendingOptionContainer = new VBox();
-        sendingOptionContainer.setSpacing(10.0);
-        Button sendButton = new Button("Send");
-        ChoiceBox<String> receiverChoice = new ChoiceBox<>();
-        sendingOptionContainer.getChildren().addAll(sendButton, receiverChoice);
-        sendMessageContainer.getChildren().add(sendingOptionContainer);
+        VBox sendingOptionContainer = initializeSendingOptionContainer(new VBox());
+        sendMessageContainer.getChildren().addAll(initializeSendMessageField(), sendingOptionContainer);
 
         popUpFiller.getChildren().addAll(receivedMessagesContainer, sendMessageContainer);
         chatPopUp.getContent().getChildren().add(popUpFiller);
+
         //Add welcome message that will be removed when the first message is received
         messages.getItems().add(new ChatMessage("Game", "Welcome to the chat!"));
     }
@@ -85,5 +85,34 @@ public class ChatDisplay {
 
     public void open() {
         chatPopUp.open();
+    }
+
+    private ChoiceBox<String> initializeReceiverChoice(){
+        receiverChoice = new ChoiceBox<>();
+        receiverChoice.getItems().add("Everyone");
+        for(String player : GUI.getLightGame().getLightGameParty().getPlayerActiveList().keySet()){
+            receiverChoice.getItems().add(player);
+        }
+        receiverChoice.setValue("Everyone");
+        return receiverChoice;
+    }
+
+    private Button initializeSendButton(){
+        //TODO implement send button, add icon
+        sendButton = new Button("Send");
+        return sendButton;
+    }
+
+    private TextField initializeSendMessageField(){
+        sendMessageField = new TextField();
+        sendMessageField.setPromptText("Write a message to: " + receiverChoice.getValue());
+        return sendMessageField;
+    }
+
+    private VBox initializeSendingOptionContainer(VBox sendingOptionContainer){
+        sendingOptionContainer.setSpacing(10.0);
+        sendingOptionContainer.setAlignment(Pos.CENTER);
+        sendingOptionContainer.getChildren().addAll(initializeReceiverChoice(), initializeSendButton());
+        return sendingOptionContainer;
     }
 }
