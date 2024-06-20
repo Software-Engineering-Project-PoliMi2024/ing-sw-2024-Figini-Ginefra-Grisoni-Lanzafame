d93ff5140618637e2807aa9a15f8bfa4148345cf
dedepivot <samuele.grisoni@gmail.com>
2024-06-20 12:57:45 +0200

    feat: add answer feature

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/Components/ChatDisplay.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/Components/ChatDisplay.java
@@ -3,6 +3,8 @@ package it.polimi.ingsw.view.GUI.Components;
 import it.polimi.ingsw.model.playerReleted.ChatMessage;
 import it.polimi.ingsw.view.GUI.Components.Utils.PopUp;
 import it.polimi.ingsw.view.GUI.GUI;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
@@ -23,13 +25,14 @@ public class ChatDisplay {
     public ChatDisplay(AnchorPane parent) {
         chatPopUp = new PopUp(parent);
         messages.setCellFactory(param -> listViewChatMessageCellFactory());
+        messages.getSelectionModel().selectedItemProperty().addListener(linkChoicetoAnswer());
 
         VBox popUpFiller = new VBox();
         popUpFiller.setSpacing(20.0);
         AnchorPane.setTopAnchor(popUpFiller, 20.0);
         AnchorPane.setBottomAnchor(popUpFiller, 20.0);
-        AnchorPane.setLeftAnchor(popUpFiller, 0.0);
-        AnchorPane.setRightAnchor(popUpFiller, 0.0);
+        AnchorPane.setLeftAnchor(popUpFiller, 20.0);
+        AnchorPane.setRightAnchor(popUpFiller, 20.0);
 
         //Create a container for received messages which will contain the ListView
         HBox receivedMessagesContainer = new HBox();
@@ -85,6 +88,23 @@ public class ChatDisplay {
 
     public void open() {
         chatPopUp.open();
+    }
+
+    public ChangeListener<ChatMessage> linkChoicetoAnswer(){
+        return new ChangeListener<ChatMessage>() {
+            @Override
+            public void changed(ObservableValue<? extends ChatMessage> observableValue, ChatMessage chatMessage, ChatMessage t1) {
+                ChatMessage selectedMessage = messages.getSelectionModel().getSelectedItem();
+                if(selectedMessage != null && !selectedMessage.getSender().equals("Game")
+                        && !selectedMessage.getSender().equals(GUI.getLightGame().getLightGameParty().getYourName())){
+                    sendMessageField.setPromptText("Write a message to: " + selectedMessage.getSender());
+                    receiverChoice.setValue(selectedMessage.getSender());
+                }else{
+                    receiverChoice.setValue("Everyone");
+                    sendMessageField.setPromptText("Write a message to: " + receiverChoice.getValue());
+                }
+            }
+        };
     }
 
     private ChoiceBox<String> initializeReceiverChoice(){
