c72656005092eb0cfd094af49e08db2fecfeca96
figinii <matteo5.figini@mail.polimi.it>
2024-06-21 16:20:31 +0200

    chore: remove unused interfaces

--- CodexNaturalis/src/main/java/it/polimi/ingsw/lightModel/LightModelUpdaterInterfaces/LightModelUpdater.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.ingsw.lightModel.LightModelUpdaterInterfaces;
-
-import java.io.Serializable;
-import java.rmi.Remote;
-
-public interface LightModelUpdater extends LightLobbyListUpdater, LightLobbyUpdater, LightGameUpdater {
-
-}
--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/ViewInterface.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/ViewInterface.java
@@ -1,6 +1,5 @@
 package it.polimi.ingsw.view;
 
-import it.polimi.ingsw.lightModel.LightModelUpdaterInterfaces.LightModelUpdater;
 import it.polimi.ingsw.lightModel.diffs.ModelDiffs;
 import it.polimi.ingsw.lightModel.lightTableRelated.LightGame;
 import it.polimi.ingsw.lightModel.lightTableRelated.LightLobby;
@@ -9,7 +8,39 @@ import it.polimi.ingsw.lightModel.lightTableRelated.LightLobbyList;
 import java.io.Serializable;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
-public interface ViewInterface extends LightModelUpdater, LoggerInterface, Serializable, Remote {
+public interface ViewInterface extends Serializable, Remote {
     void transitionTo(ViewState state) throws Exception;
+
+    /** Logs a message to the view. */
+    void log(String logMsg) throws Exception;
+
+    /** Logs an error message to the view. And renders the active commands*/
+    void logErr(String logMsg) throws Exception;
+
+    /** Logs a message to the view relative to somebody else's action. */
+    void logOthers(String logMsg) throws Exception;
+
+    /** Logs a message to the view relative to the game. */
+    void logGame(String logMsg) throws Exception;
+    void logChat(String logMsg) throws Exception;
+
+    void updateLobbyList(ModelDiffs<LightLobbyList> diff) throws Exception;
+
+    void updateLobby(ModelDiffs<LightLobby> diff) throws Exception;
+    /**
+     * Update the game with a diff
+     * @param diff the diff to apply
+     * @throws RemoteException if the connection between the server and the client fails
+     */
+    void updateGame(ModelDiffs<LightGame> diff) throws Exception;
+
+    /**
+     * Set the final ranking of the game
+     * @param ranking the ordered list of the final raking in decreasing order
+     *                depending on player points
+     * @throws RemoteException if the connection between the server and the client fails
+     */
+    void setFinalRanking(List<String> ranking) throws Exception;
 }
