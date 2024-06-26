68a3cd0390cfe9188e4243b79059f06ffd7a914d
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-06-27 01:41:15 +0200

    doc: add doc

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/Components/CodexRelated/CodexGUI.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/Components/CodexRelated/CodexGUI.java
@@ -18,24 +18,32 @@ import javafx.scene.layout.*;
 
 import java.util.*;
 
+/**
+ * This class represents the codex of the player in the GUI
+ */
 public class CodexGUI implements Observer {
+    /** The pane that contains the codex */
     private final StackPane codex = new StackPane();
-
+    /** The center of the codex */
     private Point2D center = new Point2D(0, 0);
-
+    /** The scale of the codex */
     private double scale = 1;
-
+    /** The past x position of the mouse */
     private double pastX = 0;
+    /** The past y position of the mouse */
     private double pastY = 0;
-
+    /** The list of cards in the codex */
     private final List<CardGUI> cards = new ArrayList<>();
+    /** The list of frontier cards in the codex */
     private final List<FrontierCardGUI> frontier = new ArrayList<>();
-
+    /** whether the frontier is visible or not */
     private boolean isFrontierVisible = false;
-
+    /** The pawn of the player */
     private final ImageView pawn = new ImageView();
 
-
+    /**
+     * Creates a new CodexGUI. It initializes the codex and sets the background as well as all the listeners to user input.
+     */
     public CodexGUI() {
         AnchorPane.setBottomAnchor(codex, 0.0);
         AnchorPane.setLeftAnchor(codex, 0.0);
@@ -91,6 +99,10 @@ public class CodexGUI implements Observer {
         codex.getChildren().add(pawn);
     }
 
+    /**
+     * Updates the background of the codex as a grid of tiles.
+     * The tiles are scaled according to the scale of the codex and are moved according to the center of the codex.
+     */
     private void updateBg(){
         double width = 50 * this.getScale();
         double height = width;
@@ -118,21 +130,34 @@ public class CodexGUI implements Observer {
         codex.setBackground(new Background(bgImage));
     }
 
+    /**
+     * Attaches the codex to the codex of the player and to the party.
+     */
     public void attachToCodex(){
         this.getLightCodex().attach(this);
         GUI.getLightGame().getLightGameParty().attach(this);
     }
 
+    /**
+     * Gets the light codex of the target player.
+     * @return the codex of the target player.
+     */
     protected LightCodex getLightCodex(){
         return GUI.getLightGame().getMyCodex();
     }
 
+    /**
+     * Returns the pane that contains the codex.
+     * @return the pane that contains the codex.
+     */
     public Pane getCodex() {
         return codex;
     }
 
     /**
-     * Get the position of a card in the codex.
+     * Get the position of a card in the codex space given its position in the grid space.
+     * The codex space is a space where the center of the codex is the origin and the x-axis is the horizontal axis of the grid space, expressed in pixels.
+     * The grid space is the same coordinate system used in the model where the center of the grid is the origin and the x-axis is the horizontal axis expressed in card units.
      * @param position The position of the card in Grid space
      * @return The position of the card in the codex
      */
@@ -143,6 +168,13 @@ public class CodexGUI implements Observer {
         );
     }
 
+    /**
+     * Get the position of a point in the grid space given its position in the codex space.
+     * The codex space is a space where the center of the codex is the origin and the x-axis is the horizontal axis of the grid space, expressed in pixels.
+     * The grid space is the same coordinate system used in the model where the center of the grid is the origin and the x-axis is the horizontal axis expressed in card units.
+     * @param point The point in the codex space
+     * @return The position of the point in the grid space
+     */
     public Point2D getGridPosition(Point2D point) {
         return new Point2D(
                 (point.getX() - center.getX()) / (GUIConfigs.codexGridWith) / this.getScale(),
@@ -150,6 +182,10 @@ public class CodexGUI implements Observer {
         );
     }
 
+    /**
+     * Adds a node to the codex. This method also takes care of the order of the nodes in the codex ensuring that the pawn is always on top of the start card but behind the other cards.
+     * @param node the node to add.
+     */
     public synchronized void addToCodex(Node node){
         codex.getChildren().add(node);
 
@@ -164,6 +200,11 @@ public class CodexGUI implements Observer {
 
     }
 
+    /**
+     * Adds a card to the codex. The card's position and scale are set according to the codex space.
+     * @param card the card to add.
+     * @param position the position of the card in the grid space.
+     */
     public synchronized void addCard(CardGUI card, Position position) {
         cards.add(card);
 
@@ -175,23 +216,37 @@ public class CodexGUI implements Observer {
         Point2D cardPosition = getCardPosition(position);
         card.setTranslation(cardPosition.getX(), cardPosition.getY());
         card.setScale(this.getScale());
-//        card.getImageView().setTranslateX(cardPosition.first());
-//        card.getImageView().setTranslateY(cardPosition.second());
-
     }
 
+    /**
+     * Sets the center of the codex.
+     * @param x the x coordinate of the center.
+     * @param y the y coordinate of the center.
+     */
     public void setCenter(double x, double y) {
         center = new Point2D(x, y);
     }
 
+    /**
+     * Sets the center of the codex.
+     * @param center the center of the codex.
+     */
     public void setCenter(Point2D center) {
         this.center = center;
     }
 
+    /**
+     * Returns the nickname of the target player.
+     * @return the nickname of the target player.
+     */
     public String getTargetPlayer(){
         return GUI.getLightGame().getLightGameParty().getYourName();
     }
 
+    /**
+     * Updates the scale and the position of the pawn in the codex space.
+     * @param scale the scale of the codex.
+     */
     private void setScaleAndUpdateTransformPawn(double scale){
         pawn.setScaleX(scale);
         pawn.setScaleY(scale);
@@ -200,6 +255,9 @@ public class CodexGUI implements Observer {
         pawn.setTranslateY(center.getY());
     }
 
+    /**
+     * Updates the codex according to the light codex. This is called by the Observer pattern.
+     */
     public synchronized void update(){
         int n = this.getLightCodex().getPlacementHistory().size();
 
@@ -251,6 +309,11 @@ public class CodexGUI implements Observer {
 
     }
 
+    /**
+     * Returns the frontier card that is closest to the given point.
+     * @param point the point to snap from.
+     * @return the frontier card that is closest to the given point.
+     */
     public FrontierCardGUI snapToFrontier(Point2D point){
         //Find the closest frontier position
         return this.frontier.stream().min(
@@ -262,6 +325,10 @@ public class CodexGUI implements Observer {
         ).get();
     }
 
+    /**
+     * Toggles the visibility of the frontier.
+     * @param isFrontierVisible true if the frontier is visible, false otherwise.
+     */
     public void toggleFrontier(boolean isFrontierVisible){
         this.isFrontierVisible = isFrontierVisible;
         for(FrontierCardGUI r : frontier){
@@ -269,10 +336,18 @@ public class CodexGUI implements Observer {
         }
     }
 
+    /**
+     * Returns the scale of the codex.
+     * @return the scale of the codex.
+     */
     public double getScale() {
         return scale * scale;
     }
 
+    /**
+     * Removes a card from the codex.
+     * @param card the card to remove.
+     */
     public synchronized void removeCard(CardGUI card){
         codex.getChildren().remove(card.getImageView());
         cards.remove(card);
--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/Components/CodexRelated/CollectedCollectablesOthers.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/Components/CodexRelated/CollectedCollectablesOthers.java
@@ -3,18 +3,26 @@ package it.polimi.ingsw.view.GUI.Components.CodexRelated;
 import it.polimi.ingsw.lightModel.lightPlayerRelated.LightCodex;
 import it.polimi.ingsw.view.GUI.GUI;
 
+/**
+ * This class represents the codex of a player that is not the player that is using the GUI.
+ */
 public class CollectedCollectablesOthers extends CollectedCollectablesGUI{
+    /** The player that this component refers to. */
     private final String targetPlayer;
 
+    /**
+     * Creates a new CollectedCollectablesOthers.
+     * @param targetPlayer the player that this component refers to.
+     */
     public CollectedCollectablesOthers(String targetPlayer){
         super();
         this.targetPlayer = targetPlayer;
     }
 
-    public String getTargetPlayer(){
-        return targetPlayer;
-    }
-
+    /**
+     * Returns the player that this component refers to.
+     * @return the player that this component refers to.
+     */
     @Override
     protected LightCodex getLightCodex(){
         return GUI.getLightGame().getCodexMap().get(targetPlayer);
