4515f5ac178109a7f8b6f64855d86f4231c02025
dedepivot <samuele.grisoni@mail.polimi.it>
2024-06-26 19:58:20 +0200

    jdoc: added jdoc

--- CodexNaturalis/src/main/java/it/polimi/ingsw/Configs.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Configs.java
@@ -1,9 +1,12 @@
 package it.polimi.ingsw;
 
-import java.io.IOException;
-
+/**
+ * This class contains configuration constants for the project.
+ */
 public class Configs {
-    public static Boolean debugMode = true;
+    /** flag for enabling debug mode. */
+    public static Boolean debugMode = false;
+    /** Minutes after which the gameSave is considered expired. */
     public static long gameSaveExpirationTimeMinutes = 30;
     public static String connectionLabelRMI = "connect";
     public static String CardResourcesFolderPath = "Cards/";
@@ -11,7 +14,7 @@ public class Configs {
     public static int rmiPort = 1234;
     public static int socketPort = 12345;
     public static int secondsTimeOut = 3;
-    public static int pingPongFrequency = 3;
+    public static int pingPongPeriod = 3;
     public static int lastInGameTimerSeconds = 30;
     public static int pointsToStartGameEnding = 20;
     public static int actualDeckPos = 2;
@@ -28,6 +31,5 @@ public class Configs {
     public static String gameSaveFolderName = "GameSaved";
     public static int maxPointsPerPlayer = 29;
     public static int delayBeforeLoadingGameSaves = 0;
-
     public static int logDurationTUI_millis = 5000;
 }
