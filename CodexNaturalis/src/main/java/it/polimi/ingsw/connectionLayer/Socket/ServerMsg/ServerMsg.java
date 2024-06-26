1967641653ae14678f46180b6bfccb022aa5d042
dedepivot <samuele.grisoni@mail.polimi.it>
2024-06-26 22:49:56 +0200

    jdoc: add socket objects jdoc

--- CodexNaturalis/src/main/java/it/polimi/ingsw/connectionLayer/Socket/ServerMsg/ServerMsg.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/connectionLayer/Socket/ServerMsg/ServerMsg.java
@@ -4,12 +4,31 @@ import it.polimi.ingsw.connectionLayer.Socket.NetworkMsg;
 import it.polimi.ingsw.connectionLayer.Socket.ServerHandler;
 
 public abstract class ServerMsg extends NetworkMsg {
+    /**
+     * The index of the message. This value is set by the clientHandler and allow for process each serverMsg in order
+     */
     int index;
+
+    /**
+     * Set the index of the msg
+     * @param index to be set
+     */
     public void setIndex(int index) {
         this.index = index;
     }
+
+    /**
+     * Get the index of the msg
+     * @return the index of the msg
+     */
     public int getIndex() {
         return index;
     }
+
+    /**
+     * The actual message. This method contains the logic of the message and is called by the serverHandler
+     * @param serverHandler the serverHandler that will process the message
+     * @throws Exception if an error occurs in the serverHandler
+     */
     public abstract void processMsg(ServerHandler serverHandler) throws Exception;
 }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/connectionLayer/Socket/VirtualSocket/VirtualViewSocket.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/connectionLayer/Socket/VirtualSocket/VirtualViewSocket.java
@@ -14,10 +14,19 @@ import it.polimi.ingsw.view.ViewState;
 import java.rmi.RemoteException;
 import java.util.List;
 
+/**
+ * This class runs on the server, and it represents the view on the server.
+ * Each action that the view can do is translated into a message sent to the client.
+ */
 public class VirtualViewSocket implements VirtualView {
+    /**The controller that is running on the server*/
     private ControllerInterface controller;
+    /**The object that runs the effective communication to the Client*/
     private final ClientHandler clientHandler;
 
+    /**
+     * The constructor of the class
+     */
     public VirtualViewSocket(ClientHandler clientHandler) {
         this.clientHandler = clientHandler;
     }
@@ -31,6 +40,9 @@ public class VirtualViewSocket implements VirtualView {
 
     }
 
+    /**
+     * Sets the controller
+     */
     @Override
     public void setController(ControllerInterface controller) throws RemoteException {
         this.controller = controller;
@@ -41,51 +53,91 @@ public class VirtualViewSocket implements VirtualView {
 
     }
 
+    /**
+     * Sends a transitionTo message to the client
+     * @param state the viewState to transition to
+     */
     @Override
     public void transitionTo(ViewState state) throws RemoteException {
         clientHandler.sendServerMessage(new TransitionToMsg(state));
     }
 
+    /**
+     * Sends a log message to the client
+     * @param logMsg the string to display
+     */
     @Override
     public void log(String logMsg) throws RemoteException {
         clientHandler.sendServerMessage(new LogMsg(logMsg));
     }
 
+    /**
+     * Send a logErr message to the client
+     * @param logMsg the string to display
+     */
     @Override
     public void logErr(String logMsg) throws RemoteException {
         clientHandler.sendServerMessage(new LogErrMsg(logMsg));
     }
 
+    /**
+     * Send a logOthers message to the client
+     * @param logMsg the string to display
+     */
     @Override
     public void logOthers(String logMsg) throws RemoteException {
         clientHandler.sendServerMessage(new LogOthersMsg(logMsg));
     }
 
+    /**
+     * Send a logGame message to the client
+     * @param logMsg the string to display
+     */
     @Override
     public void logGame(String logMsg) throws RemoteException {
         clientHandler.sendServerMessage(new LogGameMsg(logMsg));
     }
 
+    /**
+     * Send a logChat message to the client
+     * @param logMsg the string to display
+     */
     @Override
     public void logChat(String logMsg) throws Exception {
         clientHandler.sendServerMessage(new LogChatMsg(logMsg));
     }
 
+    /**
+     * Send a LightLobbyList diff to the client
+     * @param diff the diff to send
+     */
     @Override
     public void updateLobbyList(ModelDiffs<LightLobbyList> diff) throws RemoteException {
         clientHandler.sendServerMessage(new UpdateLobbyListMsg(diff));
     }
 
+    /**
+     * Send a LightLobby diff to the client
+     * @param diff the diff to send
+     */
     @Override
     public void updateLobby(ModelDiffs<LightLobby> diff) throws RemoteException {
         clientHandler.sendServerMessage(new UpdateLobbyMsg(diff));
     }
 
+    /**
+     * Send a LightGame diff to the client
+     * @param diff the diff to send
+     */
     @Override
     public void updateGame(ModelDiffs<LightGame> diff) throws RemoteException {
         clientHandler.sendServerMessage(new UpdateGameMsg(diff));
     }
 
+    /**
+     * Send a setFinalRanking List to the client
+     * @param ranking list of the final ranking
+     */
     @Override
     public void setFinalRanking(List<String> ranking) throws RemoteException {
         clientHandler.sendServerMessage(new SetFinalRankingMsg(ranking));
