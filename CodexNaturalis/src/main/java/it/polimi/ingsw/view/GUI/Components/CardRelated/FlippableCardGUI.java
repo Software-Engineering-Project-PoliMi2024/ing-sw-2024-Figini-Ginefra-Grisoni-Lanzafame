627fe3a0d1b4e86ce70e149276c524991c7fa836
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-06-27 01:19:07 +0200

    doc: add doc

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/Components/CardRelated/FlippableCardGUI.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/Components/CardRelated/FlippableCardGUI.java
@@ -5,32 +5,36 @@ import it.polimi.ingsw.model.cardReleted.utilityEnums.CardFace;
 
 import java.util.function.Consumer;
 
+/**
+ * This class represents a card that can be flipped by the player.
+ */
 public class FlippableCardGUI extends CardGUI{
+    /**
+     * creates a new FlippableCardGUI.
+     * @param target the card that this GUI represents.
+     */
     public FlippableCardGUI(LightCard target) {
         super(target, CardFace.FRONT);
         this.setOnTap(e -> this.flip());
     }
 
+    /**
+     * Sets the face of the card.
+     * @param face the new face of the card.
+     */
     public void setFace(CardFace face) {
         this.face = face;
         this.update();
     }
 
+    /**
+     * Flips the card.
+     */
     public void flip(){
         if(face == CardFace.FRONT)
             face = CardFace.BACK;
         else
             face = CardFace.FRONT;
         this.update();
-    }
-
-    public void addThisByFlippable(Consumer<FlippableCardGUI> parentAdder){
-        parentAdder.accept(this);
-        this.playAddingAnimation();
-    }
-
-    public void removeThisByFlippable(Consumer<FlippableCardGUI> parentRemover){
-        this.removingAnimation.setOnFinished(e -> parentRemover.accept(this));
-        this.removingAnimation.play();
     }
 }
