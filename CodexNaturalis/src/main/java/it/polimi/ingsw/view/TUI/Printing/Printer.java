ee7eb30976dbe5f1b43c03dbb829b9552a0e57f7
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-06-26 23:06:55 +0200

    doc: add jdoc

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/TUI/Printing/Printer.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/TUI/Printing/Printer.java
@@ -44,10 +44,23 @@ public class Printer {
         printable.flush();
     }
 
+    /**
+     * This method prints the content of a Queue of Printable objects to the console side by side.
+     * If a printable object has a height greater than the others, the extra rows will be filled with spaces.
+     * @param printables the Queue of Printable objects to be printed.
+     */
     public static synchronized void printStackedHorizontally(List<Printable> printables){
         printStackedHorizontally(printables, "");
     }
 
+    /**
+     * This method prints the content of a Queue of Printable objects to the given printable side by side, separated by a string.
+     * If a printable object has a height greater than the others, the extra rows will be filled with spaces.
+     * The separator is printed in every row between the content of the Printable objects.
+     * @param printable the Printable object to which the content is printed.
+     * @param printables the Queue of Printable objects to be printed.
+     * @param separator the string to be printed between the content of the Printable objects.
+     */
     public static synchronized void printStackedHorizontally(Printable printable, List<Printable> printables, String separator){
         //Find printable with the highest height
         int maxHeight = 0;
@@ -73,6 +86,13 @@ public class Printer {
         }
     }
 
+    /**
+     * This method prints the content of a Queue of Printable objects to the console side by side, separated by a string.
+     * If a printable object has a height greater than the others, the extra rows will be filled with spaces.
+     * The separator is printed in every row between the content of the Printable objects.
+     * @param printables the Queue of Printable objects to be printed.
+     * @param separator the string to be printed between the content of the Printable objects.
+     */
     public static synchronized void printStackedHorizontally(List<Printable> printables, String separator){
         Printable printable = new Printable("");
         printStackedHorizontally(printable, printables, separator);
